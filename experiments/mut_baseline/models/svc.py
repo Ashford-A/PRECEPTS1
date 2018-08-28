d1eb01905b96fdf3f83f022a4220b5d61336ba42
Michal Grzadkowski <mgrzad@gmail.com>
2018-08-28 15:21:30 -0700

    Adding more resources for mutation and transfer baseline tests.

---
+++ experiments/mut_baseline/models/svc.py
@@ -0,0 +1,80 @@
+
+from dryadic.learning.pipelines import PresencePipe
+from dryadic.learning.selection import SelectMeanVar
+
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+
+class Base(PresencePipe):
+
+    tune_priors = (
+        ('fit__C', tuple(10 ** np.linspace(-6.3, -2.8, 36))),
+        )
+
+    feat_inst = SelectMeanVar(mean_perc=95, var_perc=95)
+    norm_inst = StandardScaler()
+    fit_inst = SVC(kernel='linear', probability=True,
+                   cache_size=500, class_weight='balanced')
+
+    def __init__(self):
+        super().__init__([('feat', self.feat_inst), ('norm', self.norm_inst),
+                          ('fit', self.fit_inst)])
+
+
+class Kernel_quad(Base):
+
+    tune_priors = (
+        ('fit__C', tuple(10 ** np.linspace(-1, 5, 9))),
+        ('fit__gamma', tuple(10 ** np.array([-8.5, -7, -5.5, -4]))),
+        )
+ 
+    fit_inst = SVC(kernel='poly', degree=2, coef0=1, probability=True,
+                   cache_size=500, class_weight='balanced')
+
+
+class Kernel_cubic(Base):
+
+    tune_priors = (
+        ('fit__C', (1e-9, 1e-6, 1e-4, 1e-2, 1e0, 1e3)),
+        ('fit__gamma', (1e-6, 1e-3, 1e-2, 1e1)),
+        )
+ 
+    fit_inst = SVC(kernel='poly', degree=3, coef0=1, probability=True,
+                   cache_size=500, class_weight='balanced')
+
+
+class Big_cache(Base):
+
+    tune_priors = (
+        ('fit__C', (1e-9, 1e-6, 1e-4, 1e-2, 1e0, 1e3)),
+        ('fit__gamma', (1e-6, 1e-3, 1e-2, 1e1)),
+        )
+ 
+    fit_inst = SVC(kernel='poly', degree=3, coef0=1, probability=True,
+                   cache_size=2000, class_weight='balanced')
+
+
+class Kernel_poly(Base):
+
+    tune_priors = (
+        ('fit__degree', (2, 3, 4, 5)),
+        ('fit__coef0', (0, 2, 5)),
+        ('fit__C', (1, 5, 10)),
+        )
+ 
+    fit_inst = SVC(kernel='poly', gamma=1e-6, probability=True,
+                   cache_size=500, class_weight='balanced')
+
+
+class Kernel_radial(Base):
+
+    tune_priors = (
+        ('fit__C', tuple(10 ** np.linspace(-1.05, 5, 12))),
+        ('fit__gamma', (1e-9, 1e-7, 1e-5)),
+        )
+ 
+    fit_inst = SVC(kernel='rbf', probability=True,
+                   cache_size=500, class_weight='balanced')
+
