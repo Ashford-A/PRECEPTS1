f666e2d91b2a6094578f1c6966ed0a145008710c
Michal Grzadkowski <mgrzad@gmail.com>
2019-01-14 15:42:36 -0800

    Restructuring subvariant isolation experiment around explicit exclusive mutation class.

--- experiments/subvariant_isolate/setup_isolate.py
+++ experiments/subvariant_isolate/setup_isolate.py
@@ -13,26 +13,72 @@ else:
     base_dir = os.path.dirname(__file__)
 
 from HetMan.experiments.subvariant_isolate import *
-from HetMan.features.cohorts.tcga import MutationCohort
-from dryadic.features.mutations import MuType
+from HetMan.experiments.utilities.load_input import load_firehose_cohort
+from dryadic.features.mutations import MuType, MutComb
 
 import argparse
-import synapseclient
+from functools import reduce
+from operator import or_
 from itertools import combinations as combn
 import dill as pickle
 
 
-def load_cohort(cohort, genes, mut_levels, cv_prop=1.0):
-    syn = synapseclient.Synapse()
-    syn.cache.cache_root_dir = syn_root
-    syn.login()
+class ExMcomb(MutComb):
 
-    return MutationCohort(
-        cohort=cohort, mut_genes=genes, mut_levels=mut_levels,
-        domain_dir=domain_dir, expr_source='Firehose', var_source='mc3',
-        copy_source='Firehose', annot_file=annot_file, expr_dir=expr_dir,
-        copy_dir=copy_dir, cv_prop=cv_prop, syn=syn
-        )
+    def __new__(cls, mtree, *mtypes):
+        if isinstance(mtree, MuType):
+            all_mtype = mtree
+        else:
+            all_mtype = MuType(mtree.allkey())
+
+        obj = super().__new__(cls, *mtypes,
+                              not_mtype=all_mtype - reduce(or_, mtypes))
+        obj.all_mtype = all_mtype
+        obj.cur_level = all_mtype.cur_level
+
+        return obj
+
+    def __hash__(self):
+        value = 0x981324 ^ (len(self.mtypes) * hash(self.all_mtype))
+        value += hash(self.mtypes)
+
+        if value == -1:
+            value = -2
+
+        return value
+
+    def __getnewargs__(self):
+        return (self.all_mtype,) + tuple(self.mtypes)
+
+    def __str__(self):
+        return ' & '.join(str(mtype) for mtype in sorted(self.mtypes))
+
+    def __repr__(self):
+        return 'ONLY {}'.format(
+            ' AND '.join(repr(mtype) for mtype in sorted(self.mtypes)))
+
+    def __eq__(self, other):
+        if not isinstance(other, ExMcomb):
+            eq = False
+
+        else:
+            eq = self.all_mtype == other.all_mtype
+            eq &= self.mtypes == other.mtypes
+
+        return eq
+
+    def __lt__(self, other):
+        if not isinstance(other, ExMcomb):
+            return NotImplemented
+
+        if self.all_mtype != other.all_mtype:
+            raise ValueError("Cannot compare combinations from different "
+                             "mutation cohorts!")
+
+        return self.mtypes < other.mtypes
+
+    def get_sorted_levels(self):
+        return self.all_mtype.get_sorted_levels()
 
 
 def main():
@@ -61,7 +107,7 @@ def main():
     # expression and mutation data
     out_path = os.path.join(base_dir, 'setup', args.cohort, args.gene)
     os.makedirs(out_path, exist_ok=True)
-    cdata = load_cohort(args.cohort, [args.gene], use_lvls)
+    cdata = load_firehose_cohort(args.cohort, [args.gene], use_lvls)
 
     if args.verbose:
         print("Looking for combinations of subtypes of mutations in gene {} "
@@ -73,7 +119,7 @@ def main():
     # find combinations of up to two point mutation subtypes present in enough
     # samples in the cohort to meet the frequency cutoff criteria
     pnt_mtypes = cdata.train_mut['Point'].find_unique_subtypes(
-        max_types=500, max_combs=2, verbose=2,
+        max_types=1000, max_combs=3, verbose=2,
         sub_levels=use_lvls, min_type_size=args.samp_cutoff
         )
 
@@ -96,35 +142,30 @@ def main():
 
     # get the mutation type corresponding to the union of all mutations
     # present in the cohort, consolidate the subtypes found thus far
-    all_mtype = MuType(cdata.train_mut.allkey())
-    use_mtypes = pnt_mtypes | cna_mtypes
 
     # for each subtype, check if it is present in enough samples even after we
     # remove the samples with another mutation or alteration present
-    only_mtypes = {(mtype, ) for mtype in use_mtypes
-                   if (len(mtype.get_samples(cdata.train_mut)
-                           - (all_mtype - mtype).get_samples(cdata.train_mut))
-                       >= args.samp_cutoff)}
+    use_mtypes = pnt_mtypes | cna_mtypes
+    only_mcombs = {ExMcomb(cdata.train_mut, mtype) for mtype in use_mtypes}
+    pair_mcombs = {ExMcomb(cdata.train_mut, mtype1, mtype2)
+                   for mtype1, mtype2 in combn(use_mtypes, 2)
+                   if (mtype1 & mtype2).is_empty()}
 
-    # for each possible pair of subtypes, check if there are enough samples
-    # that have both mutations present, but none of the remaining mutations
-    comb_mtypes = {(mtype1, mtype2) for mtype1, mtype2 in combn(use_mtypes, 2)
-                   if ((mtype1 & mtype2).is_empty()
-                       and (len((mtype1.get_samples(cdata.train_mut)
-                                 & mtype2.get_samples(cdata.train_mut))
-                                - (mtype1.get_samples(cdata.train_mut)
-                                   ^ mtype2.get_samples(cdata.train_mut))
-                                - (all_mtype - mtype1 - mtype2).get_samples(
-                                    cdata.train_mut))
-                            >= args.samp_cutoff))}
+    use_mcombs = {
+        mcomb for mcomb in only_mcombs | pair_mcombs
+        if len(mcomb.get_samples(cdata.train_mut)) >= args.samp_cutoff
+        }
 
     if args.verbose:
         print("\nFound {} exclusive sub-types and {} combination sub-types "
-              "to isolate!".format(len(only_mtypes), len(comb_mtypes)))
+              "to isolate!".format(
+                  len([x for x in use_mcombs if len(x.mtypes) == 1]),
+                  len([x for x in use_mcombs if len(x.mtypes) == 2])
+                ))
 
     # save the list of found non-duplicate subtypes to file
     pickle.dump(
-        sorted(only_mtypes | comb_mtypes),
+        sorted(use_mcombs),
         open(os.path.join(out_path,
                           'mtypes_list__samps_{}__levels_{}.p'.format(
                               args.samp_cutoff, args.mut_levels)),
@@ -137,7 +178,11 @@ def main():
                                args.samp_cutoff, args.mut_levels)),
               'w') as fl:
 
-        fl.write(str(len(only_mtypes | comb_mtypes)))
+        fl.write(str(len(use_mcombs)))
+
+    with open(os.path.join(out_path, 'cohort_data__levels_{}.p'.format(
+            args.mut_levels)), 'wb') as f:
+        pickle.dump(cdata, f)
 
 
 if __name__ == '__main__':
--- experiments/subvariant_isolate/utils.py
+++ experiments/subvariant_isolate/utils.py
@@ -1,8 +1,9 @@
 
 from dryadic.features.mutations import MuType
+from .setup_isolate import ExMcomb
+
 import numpy as np
 import pandas as pd
-
 from functools import reduce
 from operator import or_, and_
 
@@ -15,47 +16,39 @@ def compare_scores(iso_df, cdata, get_similarities=True,
     pheno_dict = {mtypes: None for mtypes in iso_df.index}
     auc_list = pd.Series(index=iso_df.index, dtype=np.float)
 
-    samps = sorted(cdata.train_samps)
     if muts is None:
         muts = cdata.train_mut
-
     if all_mtype is None:
         all_mtype = MuType(muts.allkey())
+
+    samps = sorted(cdata.train_samps)
     all_pheno = np.array(muts.status(samps, all_mtype))
+
+    pheno_dict = {mcomb: np.array(muts.status(samps, mcomb))
+                  for mcomb in iso_df.index}
     pheno_dict['Wild-Type'] = ~all_pheno
 
-    for mtypes in iso_df.index:
-        rest_pheno = np.array(muts.status(samps,
-                                          all_mtype - reduce(or_, mtypes)))
-        cur_phenos = [np.array(muts.status(samps, mtype)) for mtype in mtypes]
-        and_pheno = reduce(and_, cur_phenos)
-
-        pheno_dict[mtypes] = and_pheno
-        pheno_dict[mtypes] &= ~(rest_pheno
-                                | (reduce(or_, cur_phenos) & ~and_pheno))
-
-    for cur_mtypes, iso_vals in iso_df.iterrows():
-        simil_df.loc[cur_mtypes, cur_mtypes] = 1.0
+    for cur_mcomb, iso_vals in iso_df.iterrows():
+        simil_df.loc[cur_mcomb, cur_mcomb] = 1.0
 
         none_vals = np.concatenate(iso_vals[~all_pheno].values)
-        cur_vals = np.concatenate(iso_vals[pheno_dict[cur_mtypes]].values)
-
-        auc_list[cur_mtypes] = np.greater.outer(cur_vals, none_vals).mean()
-        auc_list[cur_mtypes] += np.equal.outer(cur_vals, none_vals).mean() / 2
+        cur_vals = np.concatenate(iso_vals[pheno_dict[cur_mcomb]].values)
+        auc_list[cur_mcomb] = np.greater.outer(cur_vals, none_vals).mean()
+        auc_list[cur_mcomb] += np.equal.outer(cur_vals, none_vals).mean() / 2
 
         if get_similarities:
             cur_diff = np.subtract.outer(cur_vals, none_vals).mean()
 
             if cur_diff != 0:
-                for other_mtypes in set(iso_df.index) - {cur_mtypes}:
+                for other_mcomb in set(iso_df.index) - {cur_mcomb}:
 
                     other_vals = np.concatenate(
-                        iso_vals[pheno_dict[other_mtypes]].values)
+                        iso_vals[pheno_dict[other_mcomb]].values)
                     other_diff = np.subtract.outer(
                         other_vals, none_vals).mean()
 
-                    simil_df.loc[[cur_mtypes], [other_mtypes]] = other_diff
-                    simil_df.loc[[cur_mtypes], [other_mtypes]] /= cur_diff
+                    simil_df.loc[cur_mcomb, other_mcomb] = other_diff
+                    simil_df.loc[cur_mcomb, other_mcomb] /= cur_diff
 
     return pheno_dict, auc_list, simil_df
 
