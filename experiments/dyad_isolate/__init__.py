0e5624bde295c892ff82af40f71c7215affa4925
Michal Grzadkowski <mgrzad@gmail.com>
2019-06-26 17:36:28 -0700

    Removing outdated dyad isolation experiment.

--- experiments/dyad_isolate/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .data_dirs import firehose_dir, syn_root
-
-__all__ = ['firehose_dir', 'syn_root']
-
---
+++ experiments/variant_mutex/__init__.py
@@ -0,0 +1,7 @@
+
+from .data_dirs import (expr_dir, copy_dir, syn_root, domain_dir,
+                        annot_file, gene_list, type_file, beatAML_files)
+
+__all__ = ['expr_dir', 'copy_dir', 'syn_root', 'domain_dir',
+           'annot_file', 'gene_list', 'type_file', 'beatAML_files']
+
