6633e8cc867763c07dcb5ceb7649e031dbcdecd4
Michal Grzadkowski <mgrzad@gmail.com>
2019-07-10 11:14:41 -0700

    Reorganizing variant baseline testing around more transfer-learning based framework.

--- experiments/variant_baseline/setup_tests.py
+++ experiments/variant_baseline/setup_tests.py
@@ -6,7 +6,7 @@ sys.path.extend([os.path.join(base_dir, '../../..')])
 
 from HetMan.experiments.variant_baseline import *
 from HetMan.experiments.utilities.load_input import parse_subtypes
-from HetMan.features.cohorts.tcga import MutationCohort
+from HetMan.features.cohorts.tcga import MutationCohort, list_cohorts
 from HetMan.features.cohorts.beatAML import BeatAmlCohort
 from dryadic.features.mutations import MuType
 
@@ -14,13 +14,14 @@ import argparse
 import synapseclient
 import pandas as pd
 import dill as pickle
+import random
 
 from functools import reduce
 from operator import or_
 from itertools import product
 
 
-def get_cohort_data(cohort, expr_source=None, cv_seed=None, test_prop=0):
+def get_cohort_data(cohort, expr_source, cv_seed=None):
     syn = synapseclient.Synapse()
     syn.cache.cache_root_dir = syn_root
     syn.login()
@@ -39,13 +40,9 @@ def get_cohort_data(cohort, expr_source=None, cv_seed=None, test_prop=0):
                               expr_file=beatAML_files['expr'],
                               samp_file=beatAML_files['samps'], syn=syn,
                               annot_file=annot_file, cv_seed=cv_seed,
-                              test_prop=test_prop)
+                              test_prop=0)
 
     else:
-        if expr_source is None or expr_source == 'default':
-            raise ValueError("If using a TCGA cohort, you must provide a "
-                             "source of expression data!")
-
         source_info = expr_source.split('__')
         source_base = source_info[0]
         collapse_txs = not (len(source_info) > 1 and source_info[1] == 'txs')
@@ -57,7 +54,7 @@ def get_cohort_data(cohort, expr_source=None, cv_seed=None, test_prop=0):
             var_source='mc3', copy_source='Firehose', annot_file=annot_file,
             type_file=type_file, expr_dir=expr_sources[source_base],
             copy_dir=copy_dir, collapse_txs=collapse_txs, syn=syn,
-            cv_seed=cv_seed, test_prop=test_prop, annot_fields=['transcript'],
+            cv_seed=cv_seed, test_prop=0, annot_fields=['transcript'],
             use_types=parse_subtypes(cohort)
             )
 
@@ -76,62 +73,133 @@ def main():
         help="minimum number of affected samples needed to test a mutation"
         )
 
-    parser.add_argument('--setup_dir', type=str, default=base_dir)
+    parser.add_argument('out_dir', type=str)
     args = parser.parse_args()
-    out_path = os.path.join(args.setup_dir, 'setup')
-    cdata = get_cohort_data(args.cohort, args.expr_source)
+    base_path = os.path.join(args.out_dir.split('variant_baseline')[0],
+                             'variant_baseline')
+    coh_path = os.path.join(base_path, args.expr_source, 'setup')
 
-    with open(os.path.join(out_path, "cohort-data.p"), 'wb') as f:
-        pickle.dump(cdata, f)
+    out_tag = "{}__samps-{}".format(args.cohort, args.samp_cutoff)
+    vars_path = os.path.join(base_path, args.expr_source, out_tag, 'setup')
+    out_path = os.path.join(args.out_dir, 'setup')
 
-    # find subsets of point mutations with enough affected samples for each
-    # mutated gene in the cohort
-    vars_list = reduce(
-        or_,
-        [{MuType({('Gene', gene): mtype})
-          for mtype in muts['Point'].branchtypes(min_size=args.samp_cutoff)}
-         for gene, muts in cdata.mtree
-         if ('Scale', 'Point') in muts.allkey()], set()
+    coh_list = list_cohorts(
+        args.expr_source,
+        expr_dir=expr_sources[args.expr_source.split('__')[0]],
+        copy_dir=copy_dir
         )
+    use_feats = None
 
-    # add copy number deletions for each gene if enough samples are affected
-    vars_list |= {MuType({('Gene', gene): {('Copy', 'DeepDel'): None}})
-                  for gene, muts in cdata.mtree
-                  if (('Scale', 'Copy') in muts.allkey()
-                      and ('Copy', 'DeepDel') in muts['Copy'].allkey()
-                      and len(muts['Copy']['DeepDel']) >= args.samp_cutoff)}
+    for coh in random.sample(coh_list | {args.cohort}, k=len(coh_list) + 1):
+        coh_tag = "{}__cohort-data.p".format(coh)
 
-    # add copy number amplifications for each gene
-    vars_list |= {MuType({('Gene', gene): {('Copy', 'DeepGain'): None}})
-                  for gene, muts in cdata.mtree
-                  if (('Scale', 'Copy') in muts.allkey()
-                      and ('Copy', 'DeepGain') in muts['Copy'].allkey()
-                      and len(muts['Copy']['DeepGain']) >= args.samp_cutoff)}
+        if coh == args.cohort:
+            copy_tag = "cohort-data.p"
+        else:
+            copy_tag = "{}__cohort-data.p".format(coh)
 
-    # add all point mutations as a single mutation type for each gene if it
-    # contains more than one type of point mutation
-    vars_list |= {MuType({('Gene', gene): {('Scale', 'Point'): None}})
-                  for gene, muts in cdata.mtree
-                  if (('Scale', 'Point') in muts.allkey()
-                      and len(muts['Point'].allkey()) > 1
-                      and len(muts['Point']) >= args.samp_cutoff)}
+        if os.path.exists(os.path.join(coh_path, coh_tag)):
+            os.system("cp {} {}".format(os.path.join(coh_path, coh_tag),
+                                        os.path.join(out_path, copy_tag)))
 
-    # filter out mutations that do not have enough wild-type samples
-    vars_list = {mtype for mtype in vars_list
-                 if (len(mtype.get_samples(cdata.mtree))
-                     <= (len(cdata.get_samples()) - args.samp_cutoff))}
+            with open(os.path.join(out_path, copy_tag), 'rb') as f:
+                cdata = pickle.load(f)
 
-    # remove mutations that are functionally equivalent to another mutation
-    vars_list -= {mtype1 for mtype1, mtype2 in product(vars_list, repeat=2)
-                  if (mtype1 != mtype2 and mtype1.is_supertype(mtype2)
-                      and (mtype1.get_samples(cdata.mtree)
-                           == mtype2.get_samples(cdata.mtree)))}
+        else:
+            cdata = get_cohort_data(coh, args.expr_source)
 
-    # save the enumerated mutations, and the number of such mutations, to file
-    with open(os.path.join(out_path, "vars-list.p"), 'wb') as fl:
-        pickle.dump(sorted(vars_list), fl)
-    with open(os.path.join(out_path, "vars-count.txt"), 'w') as f:
-        f.write(str(len(vars_list)))
+            with open(os.path.join(coh_path, coh_tag), 'wb') as f:
+                pickle.dump(cdata, f)
+            with open(os.path.join(out_path, copy_tag), 'wb') as f:
+                pickle.dump(cdata, f)
+
+        if use_feats is None:
+            use_feats = set(cdata.get_features())
+        else:
+            use_feats &= set(cdata.get_features())
+
+    with open(os.path.join(out_path, "feat-list.p"), 'wb') as f:
+        pickle.dump(use_feats, f)
+
+    if os.path.exists(os.path.join(vars_path, "vars-list.p")):
+        os.system("cp {} {}".format(os.path.join(vars_path, "vars-list.p"),
+                                    os.path.join(out_path, "vars-list.p")))
+
+    else:
+        with open(os.path.join(out_path, "cohort-data.p".format(args.cohort)),
+                  'rb') as f:
+            cdata = pickle.load(f)
+
+        # find subsets of point mutations with enough affected samples
+        # for each mutated gene in the cohort
+        vars_list = reduce(
+            or_,
+            [{MuType({('Gene', gene): mtype})
+              for mtype in muts['Point'].branchtypes(
+                  min_size=args.samp_cutoff)}
+             for gene, muts in cdata.mtree
+             if ('Scale', 'Point') in muts.allkey()], set()
+            )
+
+        # add copy number deletions for each gene if enough samples
+        # are affected
+        vars_list |= {
+            MuType({('Gene', gene): {('Copy', 'DeepDel'): None}})
+            for gene, muts in cdata.mtree
+            if (('Scale', 'Copy') in muts.allkey()
+                and ('Copy', 'DeepDel') in muts['Copy'].allkey()
+                and len(muts['Copy']['DeepDel']) >= args.samp_cutoff)
+            }
+
+        # add copy number amplifications for each gene
+        vars_list |= {
+            MuType({('Gene', gene): {('Copy', 'DeepGain'): None}})
+            for gene, muts in cdata.mtree
+            if (('Scale', 'Copy') in muts.allkey()
+                and ('Copy', 'DeepGain') in muts['Copy'].allkey()
+                and len(muts['Copy']['DeepGain']) >= args.samp_cutoff)
+            }
+
+        # add all point mutations as a single mutation type for each gene
+        # if it contains more than one type of point mutation
+        vars_list |= {MuType({('Gene', gene): {('Scale', 'Point'): None}})
+                      for gene, muts in cdata.mtree
+                      if (('Scale', 'Point') in muts.allkey()
+                          and len(muts['Point'].allkey()) > 1
+                          and len(muts['Point']) >= args.samp_cutoff)}
+
+        # filter out mutations that do not have enough wild-type samples
+        vars_list = {mtype for mtype in vars_list
+                     if (len(mtype.get_samples(cdata.mtree))
+                         <= (len(cdata.get_samples()) - args.samp_cutoff))}
+
+        # remove mutations that are functionally equivalent to
+        # another mutation
+        vars_list -= {mtype1
+                      for mtype1, mtype2 in product(vars_list, repeat=2)
+                      if (mtype1 != mtype2 and mtype1.is_supertype(mtype2)
+                          and (mtype1.get_samples(cdata.mtree)
+                               == mtype2.get_samples(cdata.mtree)))}
+
+        # save the enumerated mutations, and the number of such
+        # mutations, to file
+        with open(os.path.join(vars_path, "vars-list.p"), 'wb') as fl:
+            pickle.dump(sorted(vars_list), fl)
+        with open(os.path.join(out_path, "vars-list.p"), 'wb') as fl:
+            pickle.dump(sorted(vars_list), fl)
+
+    if os.path.exists(os.path.join(vars_path, "vars-count.txt")):
+        os.system("cp {} {}".format(os.path.join(vars_path, "vars-count.txt"),
+                                    os.path.join(out_path, "vars-count.txt")))
+
+    else:
+        with open(os.path.join(out_path, "vars-list.p"), 'rb') as f:
+            vars_list = pickle.load(f)
+
+        with open(os.path.join(vars_path, "vars-count.txt"), 'w') as f:
+            f.write(str(len(vars_list)))
+        with open(os.path.join(out_path, "vars-count.txt"), 'w') as f:
+            f.write(str(len(vars_list)))
 
 
 if __name__ == '__main__':
