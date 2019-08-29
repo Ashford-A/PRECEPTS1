3ce86fed358cb2dc6462c85c86051fcea4fc7659
Michal Grzadkowski <mgrzad@gmail.com>
2019-08-28 22:11:17 -0700

    Adding explanations of mutation trees.

--- experiments/subvariant_tour/plot_gene.py
+++ experiments/subvariant_tour/plot_gene.py
@@ -7,26 +7,35 @@ sys.path.extend([os.path.join(os.path.dirname(__file__), '../../..')])
 plot_dir = os.path.join(base_dir, 'plots', 'gene')
 
 from HetMan.experiments.subvariant_tour import *
-from HetMan.experiments.subvariant_infer import variant_clrs
+from HetMan.experiments.subvariant_tour import pnt_mtype
 from HetMan.experiments.subvariant_tour.merge_tour import merge_cohort_data
+from HetMan.experiments.subvariant_tour.utils import RandomType
+from HetMan.experiments.subvariant_tour.plot_aucs import get_fancy_label
+
+from HetMan.experiments.subvariant_infer import variant_clrs
 from dryadic.features.data.domains import get_protein_domains
 from dryadic.features.mutations import MuType, MuTree
 
 import argparse
-from pathlib import Path
 import glob as glob
+from pathlib import Path
 import bz2
 import dill as pickle
 
+import numpy as np
 import pandas as pd
+
 from functools import reduce
 from operator import or_, itemgetter
 import re
+import random
 
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+import matplotlib.gridspec as gridspec
 from matplotlib.patches import Rectangle as Rect
 from matplotlib.collections import PatchCollection
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
@@ -161,6 +170,22 @@ def plot_mutation_lollipop(cdata_dict, domain_dict, args):
     plt.close()
 
 
+def clean_level(lvl):
+    if 'Domain_' in lvl:
+        use_lvl = "{} Domain".format(lvl.split('Domain_')[1])
+
+    elif '_base' in lvl:
+        use_lvl = "{}(base)".format(lvl.split('_base')[0])
+
+    elif lvl == 'Location':
+        use_lvl = "Amino Acid"
+
+    else:
+        use_lvl = lvl
+
+    return use_lvl
+
+
 def clean_label(lbl, lvl):
     if lvl == 'Exon':
         use_lbl = lbl.split('/')[0]
@@ -183,7 +208,8 @@ def clean_label(lbl, lvl):
     return use_lbl
 
 
-def recurse_labels(ax, mtree, ptchs, xlims, ymax, all_size, cur_j=0):
+def recurse_labels(ax, mtree, xlims, ymax, all_size,
+                   cur_j=0, clr_mtype=False, add_lbls=True):
     all_wdth = len(MuType(mtree.allkey()).subkeys())
     cur_x = xlims[0]
 
@@ -198,34 +224,61 @@ def recurse_labels(ax, mtree, ptchs, xlims, ymax, all_size, cur_j=0):
                 [ymax - 0.18 - cur_j, ymax + 0.19 - cur_j],
                 c='0.71', linewidth=1.3, solid_capstyle='round')
 
-        ptchs.append(Rect((cur_x + lbl_prop * 0.12, ymax - 0.8 - cur_j),
-                          lbl_prop * 0.76, 0.6))
+        if add_lbls:
+            mut_lbl = clean_label(lbl, mtree.mut_level)
+            if (lbl_prop / all_size) > 1/41 and len(tuple(mtree)) > 1:
+                if len(muts) == 1:
+                    mut_lbl = "{}\n(1 sample)".format(mut_lbl)
+                else:
+                    mut_lbl = "{}\n({} samps)".format(mut_lbl, len(muts))
 
-        mut_lbl = clean_label(lbl, mtree.mut_level)
-        if (lbl_prop / all_size) > 1/41 and len(tuple(mtree)) > 1:
-            if len(muts) == 1:
-                mut_lbl = "{}\n(1 sample)".format(mut_lbl)
+            if (lbl_prop / all_size) <= 1/51:
+                use_rot = 90
             else:
-                mut_lbl = "{}\n({} samps)".format(mut_lbl, len(muts))
+                use_rot = 0
 
-        if (lbl_prop / all_size) <= 1/51:
-            use_rot = 90
+            ax.text(cur_x + lbl_prop / 2, ymax - 0.5 - cur_j, mut_lbl,
+                    size=9 - 2.5 * cur_j, ha='center', va='center',
+                    rotation=use_rot)
+
+        if clr_mtype is False or clr_mtype == pnt_mtype:
+            use_clr = variant_clrs['Point']
+            sub_mtype = clr_mtype
+
+        elif clr_mtype is None:
+            use_clr = variant_clrs['Point']
+            sub_mtype = None
+
+        elif clr_mtype.is_empty():
+            use_clr = variant_clrs['WT']
+            sub_mtype = clr_mtype
+
+        elif clr_mtype.subtype_list()[0][0] == lbl:
+            use_clr = variant_clrs['Point']
+            sub_mtype = clr_mtype.subtype_list()[0][1]
+
         else:
-            use_rot = 0
+            use_clr = variant_clrs['WT']
+            sub_mtype = MuType({})
 
-        ax.text(cur_x + lbl_prop / 2, ymax - 0.5 - cur_j, mut_lbl,
-                size=9 - 2.5 * cur_j, ha='center', va='center',
-                rotation=use_rot)
+        if clr_mtype is False:
+            sub_lbls = True
+        else:
+            sub_lbls = False
+
+        ax.add_patch(Rect((cur_x + lbl_prop * 0.12, ymax - 0.8 - cur_j),
+                          lbl_prop * 0.76, 0.6, facecolor=use_clr,
+                          alpha=0.41, linewidth=0))
 
         if isinstance(muts, MuTree):
-            ax, ptchs = recurse_labels(ax, muts, ptchs,
-                                       (cur_x + lbl_prop * 0.06,
-                                        cur_x + lbl_prop * 0.94),
-                                       ymax, all_size, cur_j + 1)
+            ax = recurse_labels(
+                ax, muts, (cur_x + lbl_prop * 0.06, cur_x + lbl_prop * 0.94),
+                ymax, all_size, cur_j + 1, sub_mtype, sub_lbls
+                )
 
         cur_x += lbl_prop
 
-    return ax, ptchs
+    return ax
 
 
 def plot_mutation_tree(cdata_dict, domain_dict, args):
@@ -235,7 +288,7 @@ def plot_mutation_tree(cdata_dict, domain_dict, args):
     use_mtrees = [base_cdict[lvl_k].mtree[args.gene]['Point']
                   for lvl_k in lvls_key]
     lvls_list = [['Exon', 'Location', 'Protein']]
-    lvls_list += [lvl_k.split('__') for lvl_k in lvls_key[1:]]
+    lvls_list += [lvl_k.split('__') for lvl_k in sorted(lvls_key[1:])[::-1]]
 
     fig, axarr = plt.subplots(
         figsize=(14, 0.3 + 1.9 * len(lvls_key)), nrows=len(lvls_key), ncols=1,
@@ -245,27 +298,24 @@ def plot_mutation_tree(cdata_dict, domain_dict, args):
 
     for ax, use_mtree, use_lvls in zip(axarr, use_mtrees, lvls_list):
         ax.axis('off')
-        mut_ptchs = []
         leaf_count = len(MuType(use_mtree.allkey()).subkeys())
 
         for i, lvl in enumerate(use_lvls):
-            ax.text(leaf_count / -23, len(use_lvls) - i - 0.5, lvl,
-                    size=12, ha='right', va='center')
+            ax.text(leaf_count / -41, len(use_lvls) - i - 0.5,
+                    clean_level(lvl), size=12, ha='right', va='center')
 
-        ax.text(leaf_count / 2, len(use_lvls) + 0.61,
+        ax.text(leaf_count / 2, len(use_lvls) + 0.67,
                 "All {} Point Mutations\n({} samples)".format(
                     args.gene, len(use_mtree)),
                 size=11, ha='center', va='center')
 
-        mut_ptchs.append(Rect((leaf_count * 0.29, len(use_lvls) + 0.23),
-                              leaf_count * 0.42, 1.03, clip_on=False))
+        ax.add_patch(Rect((leaf_count * 0.29, len(use_lvls) + 0.23),
+                          leaf_count * 0.42, 0.91, clip_on=False,
+                          facecolor=variant_clrs['Point'], alpha=0.41,
+                          linewidth=0))
 
-        ax, mut_ptchs = recurse_labels(ax, use_mtree, mut_ptchs,
-                                       (0, leaf_count), len(use_lvls),
-                                       leaf_count)
-
-        ax.add_collection(PatchCollection(mut_ptchs, alpha=0.4, linewidth=0,
-                                          facecolor=variant_clrs['Point']))
+        ax = recurse_labels(ax, use_mtree, (0, leaf_count), len(use_lvls),
+                            leaf_count, clr_mtype=False, add_lbls=True)
 
         ax.set_xlim(0, leaf_count * 1.03)
         ax.set_ylim(0, len(use_lvls) + 1)
@@ -274,6 +324,118 @@ def plot_mutation_tree(cdata_dict, domain_dict, args):
     fig.tight_layout(h_pad=0)
     fig.savefig(os.path.join(
         plot_dir, "mut-tree_{}__{}.svg".format(args.cohort, args.gene)
+        ), bbox_inches='tight', format='svg')
+
+    plt.close()
+
+
+def plot_mutation_classif(infer_dict, out_dict, cdata_dict, args):
+    base_cdict = tuple(cdata_dict.values())[0]
+    lvls_key = ['Loc'] * 2 + sorted(
+        lvls for lvls in set(base_cdict) - {'Loc'})[::-1]
+
+    use_mtrees = [base_cdict[lvl_k].mtree[args.gene]['Point']
+                  for lvl_k in lvls_key]
+    lvls_list = [['Exon', 'Location', 'Protein']] * 2
+    lvls_list += [lvl_k.split('__') for lvl_k in lvls_key[2:]]
+
+    fig = plt.figure(figsize=(12, 0.3 + 2.1 * len(lvls_key)))
+    gs = gridspec.GridSpec(nrows=len(lvls_key), ncols=3,
+                           width_ratios=[1, 7, 4])
+
+    wild_ax = fig.add_subplot(gs[:, 0])
+    wild_ax.axis('off')
+    wild_ax.add_patch(Rect((-0.2, 0.2), 0.8, 0.6,
+                           facecolor=variant_clrs['WT'],
+                           clip_on=False, linewidth=0, alpha=0.41))
+
+    wild_ax.text(
+        0.2, 0.5, "Wild-Type for {} Point Mutations\n({} samples)".format(
+            args.gene, len(set(base_cdict['Loc'].get_samples())
+                           - set(base_cdict['Loc'].mtree[
+                               args.gene]['Point'].get_samples()))
+            ),
+        size=21, ha='center', va='center', rotation=90
+        )
+
+    for i, (use_mtree, use_lvls, lvls_k) in enumerate(zip(
+            use_mtrees, lvls_list, lvls_key)):
+        tree_ax = fig.add_subplot(gs[i, 1])
+        tree_ax.axis('off')
+
+        use_outs = [
+            (src, clf, auc_df.Chrm, phn_dict)
+            for (src, lvls, clf), (auc_df, phn_dict) in out_dict.items()
+            if lvls == '__'.join(use_lvls)
+            ]
+
+        if i == 0:
+            plt_mtype = MuType({('Gene', args.gene): pnt_mtype})
+
+        else:
+            plt_mtype = MuType({
+                ('Gene', args.gene): random.choice(sorted(
+                    base_cdict[lvls_k].mtree[
+                        args.gene]['Point'].branchtypes(min_size=25)
+                    & {mtype.subtype_list()[0][1] for mtype in use_outs[0][3]
+                       if not isinstance(mtype, RandomType)}
+                    ))
+                })
+
+        use_src, use_clf, use_auc, use_phn = sorted(
+            [(src, clf, auc_vals[plt_mtype], phn_dict[plt_mtype])
+             for (src, clf, auc_vals, phn_dict) in use_outs],
+            key=itemgetter(2)
+            )[-1]
+
+        leaf_count = len(MuType(use_mtree.allkey()).subkeys())
+        tree_ax.add_patch(Rect((leaf_count * 0.35, len(use_lvls) + 0.23),
+                               leaf_count * 0.42, 1.03, clip_on=False,
+                               facecolor=variant_clrs['Point'], alpha=0.41,
+                               linewidth=0))
+
+        for j, lvl in enumerate(use_lvls):
+            tree_ax.text(leaf_count / 13, len(use_lvls) - j - 0.5,
+                         clean_level(lvl), size=11, ha='right', va='center')
+
+        tree_ax = recurse_labels(
+            tree_ax, use_mtree,
+            (leaf_count / 11, leaf_count), len(use_lvls), leaf_count,
+            clr_mtype=plt_mtype.subtype_list()[0][1], add_lbls=False
+            )
+
+        tree_ax.set_xlim(0, leaf_count * 1.03)
+        tree_ax.set_ylim(0, len(use_lvls) + 1)
+
+        infer_vals = infer_dict[
+            use_src, '__'.join(use_lvls), use_clf]['Chrm'].loc[plt_mtype]
+        viol_ax = fig.add_subplot(gs[i, 2])
+
+        sns.violinplot(x=np.concatenate(infer_vals[~use_phn].values),
+                       ax=viol_ax, palette=[variant_clrs['WT']],
+                       linewidth=0, cut=0)
+        sns.violinplot(x=np.concatenate(infer_vals[use_phn].values),
+                       ax=viol_ax, palette=[variant_clrs['Point']],
+                       linewidth=0, cut=0)
+
+        viol_ax.text(-0.13, 1,
+                     "{}\n({} samples)".format(get_fancy_label(plt_mtype),
+                                               np.sum(use_phn)),
+                     size=13, ha='left', va='top',
+                     transform=viol_ax.transAxes)
+
+        viol_ax.text(1, -0.05, "AUC: {:.3f}".format(use_auc),
+                     size=14, ha='right', va='bottom',
+                     transform=viol_ax.transAxes)
+
+        viol_ax.get_children()[0].set_alpha(0.41)
+        viol_ax.get_children()[2].set_alpha(0.41)
+        viol_ax.set_xticklabels([])
+
+    # save the plot to file
+    fig.tight_layout(w_pad=1.1, h_pad=2.7)
+    fig.savefig(os.path.join(
+        plot_dir, "mut-classif_{}__{}.svg".format(args.cohort, args.gene)
         ), bbox_inches='tight', format='svg')
 
     plt.close()
@@ -322,7 +484,7 @@ def main():
                 base_dir, "{}__{}__samps-{}".format(src, args.cohort, ctf)
                 ), lvls, use_seed=8713)
                 for (_, lvls, _), ctf in outs.iteritems()
-                if 'Domain_' in lvls}
+                if lvls != 'Exon__Location__Protein'}
             }
         for (src, clf), outs in out_use.groupby(['Source', 'Classif'])
         }
@@ -333,7 +495,8 @@ def main():
         for domn in {lvls.split('Domain_')[1].split('__')[0]
                      for lvls in reduce(or_,
                                         [set(cdict.keys()) - {'Loc'}
-                                         for cdict in cdata_dict.values()])}
+                                         for cdict in cdata_dict.values()])
+                     if 'Domain_' in lvls}
         }
 
     infer_dict = {
@@ -342,7 +505,6 @@ def main():
             "out-data__{}__{}.p.gz".format(lvls, clf)
             ), 'r'))['Infer']
         for (src, lvls, clf), ctf in out_use.iteritems()
-        if lvls == 'Exon__Location__Protein' or 'Domain_' in lvls
         }
  
     out_dict = {
@@ -351,11 +513,11 @@ def main():
             "out-aucs__{}__{}.p.gz".format(lvls, clf)
             ), 'r'))
         for (src, lvls, clf), ctf in out_use.iteritems()
-        if lvls == 'Exon__Location__Protein' or 'Domain_' in lvls
         }
 
     plot_mutation_lollipop(cdata_dict, domn_dict, args)
     plot_mutation_tree(cdata_dict, domn_dict, args)
+    plot_mutation_classif(infer_dict, out_dict, cdata_dict, args)
 
 
 if __name__ == '__main__':
