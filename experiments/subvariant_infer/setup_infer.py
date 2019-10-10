0532eac508237c83874a00c663e3cf08b7687285
Michal Grzadkowski <mgrzad@gmail.com>
2019-10-10 10:05:16 -0700

    Updating how subgroupings and transfer cohorts are enumerated in subvariant inference.

--- experiments/subvariant_infer/setup_infer.py
+++ experiments/subvariant_infer/setup_infer.py
@@ -5,7 +5,6 @@ base_dir = os.path.dirname(__file__)
 sys.path.extend([os.path.join(base_dir, '../../..')])
 
 from HetMan.experiments.subvariant_infer import *
-from HetMan.experiments.subvariant_tour.merge_tour import merge_cohort_data
 from HetMan.features.cohorts.tcga import list_cohorts
 from HetMan.experiments.subvariant_tour.setup_tour import get_cohort_data
 
@@ -15,11 +14,11 @@ from HetMan.experiments.subvariant_infer.utils import Mcomb, ExMcomb
 from dryadic.features.mutations import MuType
 
 import argparse
-import synapseclient
 from glob import glob
 from pathlib import Path
 import bz2
 import dill as pickle
+import subprocess
 
 import numpy as np
 import pandas as pd
@@ -27,6 +26,20 @@ import pandas as pd
 import random
 from itertools import combinations as combn
 from itertools import product
+
+
+def choose_source(cohort):
+    # choose the source of expression data to use for this tumour cohort
+    if cohort == 'beatAML':
+        use_src = 'toil__gns'
+    elif cohort == 'METABRIC':
+        use_src = 'microarray'
+
+    # default to using Broad Firehose expression calls for TCGA cohorts
+    else:
+        use_src = 'Firehose'
+
+    return use_src
 
 
 def compare_lvls(lvls1, lvls2):
@@ -60,17 +73,8 @@ def main():
     # point mutations of the given gene
     args = parser.parse_args()
     use_coh = args.cohort.split('_')[0]
+    use_source = choose_source(use_coh)
     base_mtype = MuType({('Gene', args.gene): pnt_mtype})
-
-    # choose the source of expression data to use for this tumour cohort
-    if use_coh == 'beatAML':
-        use_source = 'toil__gns'
-    elif use_coh == 'METABRIC':
-        use_source = 'microarray'
-
-    # default to using Broad Firehose expression calls for TCGA cohorts
-    else:
-        use_source = 'Firehose'
 
     # find all the subvariant enumeration experiments that have run to
     # completion using the given combination of cohort and mutation classifier
@@ -93,16 +97,6 @@ def main():
                          "`subvariant_tour` experiment is run "
                          "with mutation levels `Exon__Location__Protein` "
                          "which tests genes' base mutations!")
-
-    use_ctf = int(out_df.Samps.min())
-    cdata = merge_cohort_data(os.path.join(
-        args.tour_dir, 'subvariant_tour',
-        "{}__{}__samps-{}".format(use_source, args.cohort, use_ctf),
-        ), use_seed=8713)
-
-    if args.gene not in cdata.mut_genes:
-        raise ValueError("No mutations were tested for gene "
-                         "{} in `{}`!".format(args.gene, args.tour_dir))
 
     # load bootstrapped AUCs for enumerated subgrouping mutations
     conf_dict = dict()
@@ -129,18 +123,29 @@ def main():
 
     conf_df = pd.DataFrame.from_items(zip(conf_vals.index,
                                           conf_vals.values[:, 0])).transpose()
-    for add_lvls in {('Gene', 'Scale', 'Copy') + mtype.get_sorted_levels()[1:]
-                     for mtype in conf_df.index}:
-        cdata.add_mut_lvls(add_lvls)
 
-    main_lvls = ('Gene', 'Scale', 'Copy', 'Exon', 'Location', 'Protein')
+    use_lvls = [
+        list(lvls) for lvls in (
+            {mtype.get_sorted_levels() for mtype in conf_df.index}
+            | {('Gene', 'Exon', 'Location', 'Protein')}
+            )
+        ]
+
+    cdata = get_cohort_data(args.cohort, choose_source(args.cohort),
+                            use_lvls, [args.gene], ['transcript'])
     mtype_lvlk = {mtype: cdata.choose_mtree(mtype) for mtype in conf_df.index}
-    base_confs = conf_df.loc[base_mtype]
-    conf_scores = {mtype: np.greater.outer(confs, base_confs).mean()
-                   for mtype, confs in conf_df.iterrows()}
+    main_lvls = ('Gene', 'Scale', 'Copy', 'Exon', 'Location', 'Protein')
 
+    base_confs = conf_df.loc[base_mtype].values
+    conf_scores = {mtype: np.greater.outer(confs.values, base_confs).mean()
+                   for mtype, confs in conf_df.iterrows()}
+    base_size = len(cdata.mtrees[main_lvls][args.gene]['Point'].get_samples())
+
+    use_ctf = int(out_df.Samps.min())
     brnch_mtypes = {
-        lvls: mtree[args.gene]['Point'].branchtypes(min_size=use_ctf)
+        lvls: (mtree[args.gene]['Point'].branchtypes(min_size=use_ctf)
+               if lvls not in [('Gene', 'Scale', 'Copy'), ('Gene', 'Scale')]
+               else set())
         for lvls, mtree in cdata.mtrees.items()
         }
 
@@ -153,9 +158,11 @@ def main():
         MuType({('Gene', args.gene): {
             ('Scale', 'Point'): mtype.subtype_list()[0][1]}})
         if mtype != base_mtype else base_mtype
-        for mtype, conf_sc in conf_scores.items()
-        if (mtype == base_mtype or conf_sc >= random.random()
-            or mtype in brnch_mtypes[mtype_lvlk[mtype]])
+        for mtype, conf_sc in sorted(conf_scores.items())
+        if (mtype == base_mtype
+            or (np.sum(cdata.train_pheno(mtype)) < base_size
+                and (conf_sc >= random.random()
+                     or mtype in brnch_mtypes[mtype_lvlk[mtype]])))
         }
 
     use_mtypes |= {
@@ -166,8 +173,7 @@ def main():
         }
 
     max_size = max(np.sum(cdata.train_pheno(mtype))
-                   for mtype in conf_df.index)
-    max_size += len(cdata.mtrees[main_lvls][args.gene]['Point'].get_samples())
+                   for mtype in conf_df.index) + base_size
     max_size = int(max_size / 2)
 
     use_mtypes |= {RandomType(size_dist=(use_ctf, max_size), seed=seed + 39)
@@ -208,22 +214,24 @@ def main():
                   for mtype in use_mtypes
                   if not isinstance(mtype, RandomType)}
 
+    mcomb_lvls = {mtype: (('Gene', 'Scale') if not lvls
+                          else main_lvls if lvls == ('Copy', )
+                          else ('Gene', 'Scale', 'Copy') + lvls)
+                  for mtype, lvls in mtype_lvls.items()}
+
     use_pairs = {(mtype2, mtype1) if 'Copy' in lvls1 else (mtype1, mtype2)
                  for (mtype1, lvls1), (mtype2, lvls2)
                  in combn(mtype_lvls.items(), 2)
-                 if (((('Copy' in lvls1) ^ ('Copy' in lvls2))
-                      or ((set(lvls1) & set(lvls2)) in [set(lvls1),
-                                                        set(lvls2)]
-                          and compare_lvls(lvls1, lvls2)))
+                 if ((('Copy' in lvls1) ^ ('Copy' in lvls2))
+                     and (set(lvls1) & set(lvls2)) in [set(lvls1), set(lvls2)]
+                     and compare_lvls(lvls1, lvls2)
                      and (mtype1 & mtype2).is_empty())}
 
     use_mcombs = {Mcomb(*pair) for pair in use_pairs}
-    use_mcombs |= {ExMcomb(all_mtypes[cdata.choose_mtree(pair[0])] - ex_mtype,
-                           *pair)
+    use_mcombs |= {ExMcomb(all_mtypes[mcomb_lvls[pair[0]]] - ex_mtype, *pair)
                    for pair in use_pairs for ex_mtype in ex_mtypes}
 
-    use_mcombs |= {ExMcomb(all_mtypes[cdata.choose_mtree(mtype)] - ex_mtype,
-                           mtype)
+    use_mcombs |= {ExMcomb(all_mtypes[mcomb_lvls[mtype]] - ex_mtype, mtype)
                    for mtype in use_mtypes for ex_mtype in ex_mtypes
                    if not isinstance(mtype, RandomType)}
 
@@ -233,17 +241,19 @@ def main():
 
     base_path = os.path.join(args.out_dir.split('subvariant_infer')[0],
                              'subvariant_infer')
-    coh_path = os.path.join(base_path, 'setup')
+    coh_path = os.path.join(base_path, args.gene, 'setup')
     out_path = os.path.join(args.out_dir, 'setup')
+
+    with open(os.path.join(out_path, "muts-list.p"), 'wb') as f:
+        pickle.dump(sorted(use_mtypes), f, protocol=-1)
+    with open(os.path.join(out_path, "muts-count.txt"), 'w') as fl:
+        fl.write(str(len(use_mtypes)))
 
     coh_list = list_cohorts('Firehose', expr_dir=expr_dir, copy_dir=copy_dir)
     coh_list |= {args.cohort, 'beatAML', 'METABRIC'}
-
-    syn = synapseclient.Synapse()
-    syn.cache.cache_root_dir = syn_root
-    syn.login()
-
     use_feats = set(cdata.get_features())
+
+    random.seed()
     for coh in random.sample(coh_list, k=len(coh_list)):
         coh_tag = "{}__cohort-data.p".format(coh)
 
@@ -252,39 +262,34 @@ def main():
         else:
             copy_tag = "{}__cohort-data.p".format(coh)
 
-        if coh == 'beatAML':
-            use_src = 'toil__gns'
-        elif coh == 'METABRIC':
-            use_src = 'microarray'
-        else:
-            use_src = 'Firehose'
-
         if os.path.exists(os.path.join(coh_path, coh_tag)):
             try:
                 with open(os.path.join(coh_path, coh_tag), 'rb') as f:
                     trnsf_cdata = pickle.load(f)
 
             except EOFError:
-                trnsf_cdata = get_cohort_data(coh, use_src)
-
-            else:
-                trnsf_cdata = get_cohort_data(coh, use_src)
+                trnsf_cdata = get_cohort_data(
+                    coh, choose_source(coh),
+                    use_lvls, [args.gene], ['transcript']
+                    )
 
         else:
-            trnsf_cdata = get_cohort_data(coh, use_src)
+            trnsf_cdata = get_cohort_data(
+                coh, choose_source(coh),
+                use_lvls, [args.gene], ['transcript']
+                )
 
         use_feats &= set(trnsf_cdata.get_features())
         with open(os.path.join(coh_path, coh_tag), 'wb') as f:
             pickle.dump(trnsf_cdata, f, protocol=-1)
-        with open(os.path.join(out_path, copy_tag), 'wb') as f:
-            pickle.dump(trnsf_cdata, f, protocol=-1)
+
+        copy_prc = subprocess.run(
+            ['cp', os.path.join(coh_path, coh_tag),
+             os.path.join(out_path, copy_tag)], check=True
+            )
 
     with open(os.path.join(out_path, "feat-list.p"), 'wb') as f:
         pickle.dump(use_feats, f, protocol=-1)
-    with open(os.path.join(out_path, "muts-list.p"), 'wb') as f:
-        pickle.dump(sorted(use_mtypes), f, protocol=-1)
-    with open(os.path.join(out_path, "muts-count.txt"), 'w') as fl:
-        fl.write(str(len(use_mtypes)))
 
 
 if __name__ == '__main__':
