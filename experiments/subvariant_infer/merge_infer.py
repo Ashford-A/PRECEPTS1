d4f94f6322be29117734280c9eff4617e0f9209b
Michal Grzadkowski <mgrzad@gmail.com>
2019-10-17 18:31:39 -0700

    Updating how output data in subvariant inference pipeline is consolidated.

--- experiments/subvariant_infer/merge_infer.py
+++ experiments/subvariant_infer/merge_infer.py
@@ -4,19 +4,37 @@ import sys
 base_dir = os.path.dirname(__file__)
 sys.path.extend([os.path.join(base_dir, '../../..')])
 
-from HetMan.experiments.variant_baseline.merge_tests import MergeError
+from HetMan.experiments.subvariant_infer import *
 from HetMan.experiments.subvariant_tour.utils import RandomType
 from HetMan.experiments.subvariant_infer.utils import Mcomb, ExMcomb
+from HetMan.experiments.subvariant_tour.merge_tour import compare_muts
+from HetMan.experiments.subvariant_infer import copy_mtype
 from dryadic.features.mutations import MuType
+
+from HetMan.features.cohorts.metabric import load_metabric_samps
+from HetMan.features.cohorts.metabric import (
+    choose_subtypes as choose_metabric_subtypes)
 
 import numpy as np
 import pandas as pd
-from sklearn.externals.joblib import Parallel, delayed
+from joblib import Parallel, delayed
 
 import argparse
-from glob import glob
+from pathlib import Path
 import bz2
 import dill as pickle
+
+
+def get_cohort_subtypes(coh):
+    if coh == 'METABRIC':
+        metabric_samps = load_metabric_samps(metabric_dir)
+        subt_dict = {subt: choose_metabric_subtypes(metabric_samps, subt)
+                     for subt in ('nonbasal', 'luminal')}
+
+    else:
+        subt_dict = dict()
+
+    return subt_dict
 
 
 def cdict_hash(cdict):
@@ -32,7 +50,7 @@ def merge_cohort_dict(out_dir, use_seed=None):
             cur_cdict = pickle.load(fl)
             cur_hash = cdict_hash(cur_cdict)
 
-    new_files = glob(os.path.join(out_dir, "cohort-dict__*__*.p"))
+    new_files = Path(out_dir).glob("cohort-dict__*__*.p")
     new_mdls = [new_file.split("cohort-dict__")[1].split(".p")[0]
                 for new_file in new_files]
 
@@ -85,28 +103,27 @@ def merge_cohort_dict(out_dir, use_seed=None):
     return use_cdict
 
 
-def calculate_simls(pheno_dict, all_vals, iso_vals, cur_mtype):
-    print(cur_mtype)
-    wt_all_vals = np.concatenate(all_vals.iloc[
-        0, ~pheno_dict[cur_mtype]].values)
-    cur_all_vals = np.concatenate(all_vals.iloc[
-        0, pheno_dict[cur_mtype]].values)
+def calculate_simls(pheno_dict, cur_mtype, all_vals, iso_vals, mut_list=None):
+    if mut_list is None:
+        mut_list = tuple(pheno_dict)
 
-    none_vals = np.concatenate(iso_vals.iloc[
-        0, pheno_dict['Wild-Type']].values)
-    cur_iso_vals = np.concatenate(iso_vals.iloc[
-        0, pheno_dict[cur_mtype]].values)
+    wt_all_vals = np.concatenate(all_vals[~pheno_dict[cur_mtype]])
+    cur_all_vals = np.concatenate(all_vals[pheno_dict[cur_mtype]])
+    none_vals = np.concatenate(iso_vals[pheno_dict['Wild-Type']])
+    cur_iso_vals = np.concatenate(iso_vals[pheno_dict[cur_mtype]])
 
-    if (not isinstance(cur_mtype, RandomType)
-            or isinstance(cur_mtype.size_dist, int)):
-        assert (((len(wt_all_vals), len(cur_all_vals))
-                 / np.bincount(pheno_dict[cur_mtype]))
-                == np.array([20, 20])).all()
+    if not isinstance(cur_mtype, (RandomType, Mcomb, ExMcomb)):
+        assert (len(wt_all_vals)
+                / (~pheno_dict[cur_mtype]).sum()) in [20., 1.], cur_mtype
+        assert (len(cur_all_vals)
+                / pheno_dict[cur_mtype].sum()) in [20., 1.], cur_mtype
 
-        assert (((len(none_vals), len(cur_iso_vals))
-                 / np.array([np.sum(pheno_dict['Wild-Type']),
-                             np.sum(pheno_dict[cur_mtype])]))
-                == np.array([20, 20])).all()
+        """
+        assert (len(none_vals)
+                / (~pheno_dict['Wild-Type']).sum()) in [20., 1.], cur_mtype
+        assert (len(cur_iso_vals)
+                / pheno_dict[cur_mtype].sum()) in [20., 1.], cur_mtype
+        """
 
     all_auc = np.greater.outer(cur_all_vals, wt_all_vals).mean()
     all_auc += np.equal.outer(cur_all_vals, wt_all_vals).mean() / 2
@@ -116,15 +133,13 @@ def calculate_simls(pheno_dict, all_vals, iso_vals, cur_mtype):
     siml_dict = {cur_mtype: 1}
     cur_diff = np.subtract.outer(cur_iso_vals, none_vals).mean()
     other_mtypes = [
-        mtype for mtype in set(pheno_dict) - {cur_mtype, 'Wild-Type'}
+        mtype for mtype in set(mut_list) - {cur_mtype, 'Wild-Type'}
         if not isinstance(mtype, RandomType)
         ]
 
     if cur_diff != 0 and not isinstance(cur_mtype, RandomType):
         for other_mtype in other_mtypes:
-            other_vals = np.concatenate(iso_vals.iloc[
-                0, pheno_dict[other_mtype]].values)
-
+            other_vals = np.concatenate(iso_vals[pheno_dict[other_mtype]])
             siml_dict[other_mtype] = np.subtract.outer(
                 other_vals, none_vals).mean() / cur_diff
 
@@ -134,31 +149,103 @@ def calculate_simls(pheno_dict, all_vals, iso_vals, cur_mtype):
     return all_auc, iso_auc, siml_dict
 
 
-def compare_scores(infer_df, samps, muts_dict):
-    use_gene = {mtype.subtype_list()[0][0]
-                for _, mtype in infer_df['All'].index
+def compare_scores(infer_df, cdata):
+    use_gene = {mtype.get_labels()[0]
+                for mtype in infer_df['All'].index
                 if not isinstance(mtype, (ExMcomb, Mcomb, RandomType))}
 
     assert len(use_gene) == 1, ("Mutations to merge are not all associated "
                                 "with the same gene!")
     use_gene = tuple(use_gene)[0]
-    base_muts = tuple(muts_dict.values())[0]
-    all_mtype = MuType({('Gene', use_gene): base_muts[use_gene].allkey()})
 
-    pheno_dict = {
-        mtype: np.array(muts_dict[lvls].status(samps, mtype))
-        if lvls in muts_dict else np.array(base_muts.status(samps, mtype))
-        for lvls, mtype in infer_df['All'].index
-        }
-    pheno_dict['Wild-Type'] = ~np.array(base_muts.status(samps, all_mtype))
+    base_lvls = 'Gene', 'Scale', 'Copy', 'Exon', 'Location', 'Protein'
+    if base_lvls not in cdata.mtrees:
+        cdata.add_mut_lvls(base_lvls)
+
+    pheno_dict = {mtype: np.array(cdata.train_pheno(mtype))
+                  for mtype in infer_df['All'].index}
+    pheno_dict['Wild-Type'] = ~np.array(cdata.train_pheno(
+        MuType(cdata.mtrees[base_lvls].allkey())))
 
     siml_vals = dict(zip(infer_df['All'].index, Parallel(
         backend='threading', n_jobs=12, pre_dispatch=12)(
             delayed(calculate_simls)(
-                pheno_dict, infer_df['All'].loc[[(lvls, cur_mtype)]],
-                infer_df['Iso'].loc[[(lvls, cur_mtype)]], cur_mtype
+                pheno_dict, cur_mtype, infer_df['All'].loc[cur_mtype].values,
+                infer_df['Iso'].loc[cur_mtype].values
                 )
-            for lvls, cur_mtype in infer_df['All'].index
+            for cur_mtype in infer_df['All'].index
+            )
+        ))
+
+    auc_df = pd.DataFrame(
+        {mut: [all_auc, iso_auc]
+         for mut, (all_auc, iso_auc, _) in siml_vals.items()},
+        index=['All', 'Iso']
+        ).transpose()
+
+    simil_df = pd.DataFrame(
+        {mut: siml_dict
+         for mut, (_, _, siml_dict) in siml_vals.items()}
+        ).transpose()
+
+    return pheno_dict, auc_df, simil_df
+
+
+def transfer_signatures(trnsf_cdata, orig_cdata, mtype_list, subt_smps,
+                        all_df, iso_df):
+    use_muts = {mtype for mtype in mtype_list
+                if not isinstance(mtype, RandomType)}
+
+    use_gene = {mtype.get_labels()[0] for mtype in use_muts
+                if not isinstance(mtype, (ExMcomb, Mcomb))}
+    assert len(use_gene) == 1, ("Mutations to merge are not all associated "
+                                "with the same gene!")
+    use_gene = tuple(use_gene)[0]
+
+    if 'Copy' in trnsf_cdata.muts:
+        base_lvls = 'Gene', 'Scale', 'Copy', 'Exon', 'Location', 'Protein'
+
+    else:
+        base_lvls = 'Gene', 'Exon', 'Location', 'Protein'
+
+        use_muts = {
+            mut for mut in use_muts
+            if ((isinstance(mut, Mcomb)
+                 and not any(not (copy_mtype
+                                  & mtype.subtype_list()[0][1]).is_empty()
+                             for mtype in mut.mtypes))
+                or (isinstance(mut, ExMcomb)
+                    and (copy_mtype
+                         & mut.all_mtype.subtype_list()[0][1]).is_empty())
+                or (not isinstance(mut, (Mcomb, ExMcomb))
+                    and (copy_mtype & mut.subtype_list()[0][1]).is_empty()))
+            }
+
+    if base_lvls not in trnsf_cdata.mtrees:
+        trnsf_cdata.add_mut_lvls(base_lvls)
+
+    sub_stat = np.array([smp in orig_cdata.get_train_samples()
+                         for smp in trnsf_cdata.get_train_samples()])
+    if subt_smps:
+        sub_stat |= np.array([smp not in subt_smps
+                              for smp in trnsf_cdata.get_train_samples()])
+
+    pheno_dict = {mtype: np.array(trnsf_cdata.train_pheno(mtype))[~sub_stat]
+                  for mtype in use_muts}
+
+    pheno_dict['Wild-Type'] = ~np.array(trnsf_cdata.train_pheno(
+        MuType(trnsf_cdata.mtrees[base_lvls].allkey())))[~sub_stat]
+    use_muts = {mtype for mtype in use_muts if pheno_dict[mtype].sum() >= 20}
+
+    siml_vals = dict(zip(use_muts, Parallel(
+        backend='threading', n_jobs=12, pre_dispatch=12)(
+            delayed(calculate_simls)(
+                pheno_dict, cur_mtype,
+                all_df.loc[cur_mtype][~sub_stat].reshape(-1, 1),
+                iso_df.loc[cur_mtype][~sub_stat].reshape(-1, 1),
+                mut_list=use_muts
+                )
+            for cur_mtype in use_muts
             )
         ))
 
@@ -181,13 +268,18 @@ def main():
     parser.add_argument('use_dir', type=str, default=base_dir)
     args = parser.parse_args()
 
-    file_list = glob(os.path.join(args.use_dir, 'output', "out_task-*.p"))
+    file_list = tuple(Path(os.path.join(args.use_dir, 'output')).glob(
+        "out_task-*.p"))
     base_names = [os.path.basename(fl).split('out_')[1] for fl in file_list]
     task_ids = [int(nm.split('task-')[1].split('.p')[0]) for nm in base_names]
 
-    muts_list = pickle.load(open(os.path.join(
-        args.use_dir, 'setup', "muts-list.p"), 'rb'))
-    out_data = [pickle.load(open(fl, 'rb')) for fl in file_list]
+    with open(os.path.join(args.use_dir, 'setup', "muts-list.p"), 'rb') as f:
+        muts_list = pickle.load(f)
+
+    out_data = [None for _ in file_list]
+    for i, fl in enumerate(file_list):
+        with open(fl, 'rb') as f:
+            out_data[i] = pickle.load(f)
 
     use_clfs = set(out_dict['Clf'] for out_dict in out_data)
     assert len(use_clfs) == 1, ("Each experiment must be run with "
@@ -199,53 +291,84 @@ def main():
 
     out_dfs = {k: {
         smps: pd.concat([
-            pd.DataFrame.from_records({mut: vals[smps]
-                                       for mut, vals in out_dict[k].items()})
+            pd.DataFrame.from_dict({mtype: vals[smps]
+                                    for mtype, vals in out_dict[k].items()},
+                                   orient='index')
             for out_dict in out_data
-            ], axis=1, sort=True).transpose()
+            ])
         for smps in ['All', 'Iso']
         }
-        for k in ['Infer', 'Tune']}
-    print('assert!')
+        for k in ['Infer', 'Pars', 'Time', 'Acc', 'Transfer']}
 
-    assert (set(out_dfs['Infer']['All'].index)
-            == set(out_dfs['Infer']['Iso'].index)), (
-                "Mutations with inferred scores in naive mode do not match "
-                "the mutations with scores in isolation mode!"
+    for smps in ['All', 'Iso']:
+        assert compare_muts(out_dfs['Infer'][smps].index, muts_list), (
+            "Mutations for which predictions were made do not match the list "
+            "of mutation enumerated during setup!"
+            )
+
+    for out_k in ['Infer', 'Pars', 'Time', 'Acc']:
+        assert compare_muts(
+            out_dfs[out_k]['All'].index, out_dfs[out_k]['Iso'].index), (
+                "Mutations with inferred `{}` in naive mode do not match the "
+                "mutations with scores in isolation mode!".format(out_k)
                 )
 
-    assert (set(out_dfs['Tune']['All'].index)
-            == set(out_dfs['Tune']['Iso'].index)), (
-                "Mutations with tuned hyper-parameters in naive mode do not "
-                "match the mutations tuned in isolation mode!"
-                )
-
-    assert (set(out_dfs['Infer']['All'].index)
-            == set(out_dfs['Tune']['Iso'].index)), (
-                "Mutations with tuned hyper-parameters do not match the "
-                "mutations with inferred scores in isolation mode!"
-                )
-
-    assert out_dfs['Infer']['All'].shape[0] == len(muts_list), (
-        "Inferred scores missing for some tested mutations!")
+    assert compare_muts(
+        *[out_dfs[out_k]['All'].index
+          for out_k in ['Infer', 'Pars', 'Time', 'Acc']]
+        ), "Mutations across different output types do not match!"
 
     with bz2.BZ2File(os.path.join(args.use_dir, "out-data.p.gz"), 'w') as fl:
-        pickle.dump({'Infer': out_dfs['Infer'],
-                     'Tune': pd.concat(out_dfs['Tune'], axis=1),
-                     'Clf': tuple(use_clfs)[0]}, fl, protocol=-1)
+        pickle.dump(
+            {'Infer': out_dfs['Infer'],
+             'Pars': pd.concat(out_dfs['Pars'], axis=1),
+             'Time': pd.concat(out_dfs['Time'], axis=1),
+             'Acc': pd.concat(out_dfs['Acc'], axis=1),
+             'Transfer': out_dfs['Transfer'], 'Clf': tuple(use_clfs)[0]},
+            fl, protocol=-1
+            )
 
     with open(os.path.join(args.use_dir,
-                           'setup', "cohort-dict.p"), 'rb') as fl:
-        cdata_dict = pickle.load(fl)
+                           'setup', "cohort-data.p"), 'rb') as fl:
+        cdata = pickle.load(fl)
 
-    out_list = compare_scores(
-        out_dfs['Infer'],
-        sorted(cdata_dict['Exon__Location__Protein'].get_train_samples()),
-        {lvls: cdata.mtree for lvls, cdata in cdata_dict.items()}
-        )
-
+    out_list = compare_scores(out_dfs['Infer'], cdata)
     with bz2.BZ2File(os.path.join(args.use_dir, "out-simil.p.gz"), 'w') as fl:
         pickle.dump(out_list, fl, protocol=-1)
+
+    coh_files = Path(os.path.join(args.use_dir, 'setup')).glob(
+        "*__cohort-data.p")
+    coh_dict = {coh_fl.stem.split('__')[0]: coh_fl for coh_fl in coh_files}
+    subt_dict = dict()
+
+    for coh in tuple(coh_dict):
+        subt_dict[coh] = None
+
+        coh_subtypes = get_cohort_subtypes(coh)
+        for subt, sub_smps in coh_subtypes.items():
+            coh_dict['_'.join([coh, subt])] = coh_dict[coh]
+            subt_dict['_'.join([coh, subt])] = sub_smps
+
+    trnsf_dict = {coh: {'Samps': None, 'AUC': None} for coh in coh_dict}
+    for coh, coh_fl in coh_dict.items():
+        with open(coh_fl, 'rb') as f:
+            trnsf_cdata = pickle.load(f)
+
+        trnsf_dict[coh]['Samps'] = trnsf_cdata.get_train_samples()
+        if any(mtree != dict() for mtree in trnsf_cdata.mtrees.values()):
+            if subt_dict[coh] is None:
+                coh_k = coh
+            else:
+                coh_k = coh.split('_')[0]
+
+            trnsf_dict[coh]['AUC'] = transfer_signatures(
+                trnsf_cdata, cdata, muts_list, subt_dict[coh],
+                out_dfs['Transfer']['All'][coh_k],
+                out_dfs['Transfer']['Iso'][coh_k]
+                )
+
+    with bz2.BZ2File(os.path.join(args.use_dir, "out-trnsf.p.gz"), 'w') as fl:
+        pickle.dump(trnsf_dict, fl, protocol=-1)
 
 
 if __name__ == "__main__":
