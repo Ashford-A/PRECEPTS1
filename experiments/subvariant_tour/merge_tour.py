9c4f59f627c8271f43aea27198ff7dedc81bfc38
Michal Grzadkowski <mgrzad@gmail.com>
2020-01-06 19:24:10 -0800

    Updating how subvariant touring output is consolidated.

--- experiments/subvariant_tour/merge_tour.py
+++ experiments/subvariant_tour/merge_tour.py
@@ -5,7 +5,7 @@ base_dir = os.path.dirname(__file__)
 sys.path.extend([os.path.join(base_dir, '..', '..', '..')])
 
 import argparse
-from glob import glob
+from pathlib import Path
 import bz2
 import dill as pickle
 
@@ -16,6 +16,9 @@ import random
 
 from HetMan.experiments.subvariant_tour import cis_lbls
 from HetMan.experiments.subvariant_tour.utils import RandomType
+from HetMan.experiments.subvariant_test.merge_test import (
+    compare_muts, calculate_auc)
+
 from itertools import product, cycle
 from itertools import combinations as combn
 
@@ -115,145 +118,187 @@ def merge_cohort_data(out_dir, use_seed=None):
     return use_cdata
 
 
-def compare_muts(*muts_lists):
-    return len(set(tuple(sorted(muts_list))
-                   for muts_list in muts_lists)) == 1
-
-
-def calculate_auc(pheno_dict, infer_vals, mtype, sub_indx=None):
-    if sub_indx is None:
-        mut_phn = pheno_dict[mtype]
-        wt_phn = ~pheno_dict[mtype]
-    else:
-        mut_phn = pheno_dict[mtype] & sub_indx
-        wt_phn = ~pheno_dict[mtype] & sub_indx
-
-    if mut_phn.any() and wt_phn.any():
-        auc_val = np.greater.outer(
-            np.concatenate(infer_vals.values[mut_phn]),
-            np.concatenate(infer_vals.values[wt_phn])
-            ).mean()
-        
-        auc_val += 0.5 * np.equal.outer(
-            np.concatenate(infer_vals.values[mut_phn]),
-            np.concatenate(infer_vals.values[wt_phn])
-            ).mean()
-
-    else:
-        auc_val = 0.5
-
-    return auc_val
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('use_dir', type=str, default=base_dir)
     args = parser.parse_args()
 
-    file_list = glob(os.path.join(args.use_dir, 'output', "out_task-*.p"))
-    base_names = [os.path.basename(fl).split('out_')[1] for fl in file_list]
-    task_ids = [int(nm.split('task-')[1].split('.p')[0]) for nm in base_names]
+    # get list of output files from all parallelized jobs
+    file_list = tuple(Path(os.path.join(args.use_dir, 'output')).glob(
+        "out__cv-*_task*.p"))
+    assert (len(file_list) % 40) == 0, "Missing output files detected!"
+
+    task_count = len(file_list) // 40
+    out_data = [[None for task_id in range(task_count)]
+                for cv_id in range(40)]
+
+    for out_fl in file_list:
+        base_name = out_fl.stem.split('out__')[1]
+        cv_id = int(base_name.split('cv-')[1].split('_')[0])
+        task_id = int(base_name.split('task-')[1])
+ 
+        with open(out_fl, 'rb') as f:
+            out_data[cv_id][task_id] = pickle.load(f)
+
+    use_clfs = set(out_dict['Clf'] for ols in out_data for out_dict in ols)
+    assert len(use_clfs) == 1, ("Each experiment must be run with "
+                                "exactly one classifier!")
+    use_clf = tuple(use_clfs)[0]
+
+    use_tune = set(out_dict['Clf'].tune_priors for ols in out_data
+                   for out_dict in ols)
+    assert len(use_tune) == 1, ("Each experiment must be run with "
+                                "exactly one set of tuning priors!")
+
+    with open(os.path.join(args.use_dir, '..', '..', "cohort-data.p"),
+              'rb') as fl:
+        cdata = pickle.load(fl)
+
+    out_dfs = {k: {cis_lbl: [None for cv_id in range(40)]
+                   for cis_lbl in cis_lbls}
+               for k in ['Pred', 'Pars', 'Time', 'Acc']}
+
+    for cv_id, ols in enumerate(out_data):
+        for k in out_dfs:
+            for cis_lbl in cis_lbls:
+                out_dfs[k][cis_lbl][cv_id] = pd.concat([
+                    pd.DataFrame.from_dict(
+                        {mtype: out_vals[cis_lbl]
+                         for mtype, out_vals in out_dict[k].items()},
+                        orient='index'
+                        )
+                    for out_dict in ols
+                    ])
+
+        cdata_samps = sorted(cdata.get_samples())
+        random.seed((cv_id // 4) * 7712 + 13)
+        random.shuffle(cdata_samps)
+
+        cdata.update_split(9073 + 97 * cv_id,
+                           test_samps=cdata_samps[(cv_id % 4)::4])
+        test_samps = cdata.get_test_samples()
+
+        for cis_lbl in cis_lbls:
+            out_dfs['Pred'][cis_lbl][cv_id].columns = test_samps
+
+    pred_dfs = {cis_lbl: pd.concat(pred_mats, axis=1)
+                for cis_lbl, pred_mats in out_dfs['Pred'].items()}
+
+    for cis_lbl, pred_df in pred_dfs.items():
+        assert all(smp in pred_df.columns for smp in cdata.get_samples()), (
+            "Missing mutation scores for some samples in the cohort!")
+        assert (pred_df.columns.value_counts() == 10).all(), (
+            "Inconsistent number of CV scores across cohort samples!")
+
+    pars_dfs = {cis_lbl: pd.concat(pars_mats, axis=1)
+                for cis_lbl, pars_mats in out_dfs['Pars'].items()}
+    for cis_lbl, pars_df in pars_dfs.items():
+        assert pars_df.shape[1] == (40 * len(use_clf.tune_priors)), (
+            "Tuned parameter values missing for some CVs!")
+
+    time_dfs = {cis_lbl: pd.concat(time_mats, axis=1)
+                for cis_lbl, time_mats in out_dfs['Time'].items()}
+
+    for cis_lbl, time_df in time_dfs.items():
+        assert time_df.shape[1] == 80, (
+            "Model fitting times missing for some CVs!")
+        assert (time_df.applymap(len) == use_clf.test_count).values.all(), (
+            "Model fitting times missing for some hyper-parameter values!")
+
+    acc_dfs = {cis_lbl: pd.concat(acc_mats, axis=1)
+               for cis_lbl, acc_mats in out_dfs['Acc'].items()}
+
+    for cis_lbl, acc_df in acc_dfs.items():
+        assert acc_df.shape[1] == 120, (
+            "Algorithm tuning accuracies missing for some CVs!")
+        assert (acc_df.applymap(len) == use_clf.test_count).values.all(), (
+            "Algorithm tuning stats missing for some hyper-parameter values!")
 
     with open(os.path.join(args.use_dir, 'setup', "muts-list.p"), 'rb') as f:
         muts_list = pickle.load(f)
 
-    out_data = [None for _ in file_list]
-    for i, fl in enumerate(file_list):
-        with open(fl, 'rb') as f:
-            out_data[i] = pickle.load(f)
-
-    use_clfs = set(out_dict['Clf'] for out_dict in out_data)
-    assert len(use_clfs) == 1, ("Each experiment must be run with "
-                                "exactly one classifier!")
-
-    use_tune = set(out_dict['Clf'].tune_priors for out_dict in out_data)
-    assert len(use_tune) == 1, ("Each experiment must be run with "
-                                "exactly one set of tuning priors!")
-
-
-    out_dfs = {k: {
-        cis_lbl: pd.concat([
-            pd.DataFrame.from_dict({mtype: vals[cis_lbl]
-                                    for mtype, vals in out_dict[k].items()},
-                                   orient='index')
-            for out_dict in out_data
-            ])
-        for cis_lbl in cis_lbls
-        }
-        for k in ['Infer', 'Pars', 'Time', 'Acc']}
-
-    for cis_lbl in cis_lbls:
-        assert compare_muts(out_dfs['Infer'][cis_lbl].index, muts_list), (
+    for cis_lbl, pred_df in pred_dfs.items():
+        assert compare_muts(pred_df.index, muts_list), (
             "Mutations for which predictions were made do not match the list "
             "of mutations enumerated during setup!"
             )
 
     for cis_lbl1, cis_lbl2 in combn(cis_lbls, 2):
         assert compare_muts(
-            out_dfs['Infer'][cis_lbl1].index,
-            out_dfs['Infer'][cis_lbl2].index,
-            out_dfs['Time'][cis_lbl1].index,
-            out_dfs['Time'][cis_lbl2].index
+            pred_dfs[cis_lbl1].index, pred_dfs[cis_lbl2].index,
+            time_dfs[cis_lbl1].index, time_dfs[cis_lbl2].index
             ), ("Mutations tested using cis-exclusion strategy {} do "
                 "not match those tested using strategy {}!".format(
                     cis_lbl1, cis_lbl2))
 
     for cis_lbl1, cis_lbl2 in product(cis_lbls, repeat=2):
         assert compare_muts(
-            out_dfs['Infer'][cis_lbl1].index, out_dfs['Pars'][cis_lbl2].index,
-            out_dfs['Time'][cis_lbl1].index, out_dfs['Acc'][cis_lbl2].index
+            pred_dfs[cis_lbl1].index, pars_dfs[cis_lbl2].index,
+            time_dfs[cis_lbl1].index, acc_dfs[cis_lbl2].index
             ), ("Mutations with predicted scores do not match those for "
                 "which tuned hyper-parameter values are available!")
 
-    with bz2.BZ2File(os.path.join(args.use_dir, "out-data.p.gz"), 'w') as fl:
-        pickle.dump({'Infer': out_dfs['Infer'],
-                     'Pars': pd.concat(out_dfs['Pars'], axis=1),
-                     'Time': pd.concat(out_dfs['Time'], axis=1),
-                     'Acc': pd.concat(out_dfs['Acc'], axis=1),
-                     'Clf': tuple(use_clfs)[0]}, fl, protocol=-1)
+    pred_dfs = {
+        cis_lbl: pd.DataFrame({
+            mtype: pred_df.loc[mtype].groupby(level=0).apply(
+                lambda x: x.values)
+            for mtype in muts_list
+            }).transpose()
+        for cis_lbl, pred_df in pred_dfs.items()
+        }
 
-    with open(os.path.join(args.use_dir, '..', '..',
-                           "cohort-data.p"), 'rb') as fl:
-        cdata = pickle.load(fl)
+    with bz2.BZ2File(os.path.join(args.use_dir, "out-pred.p.gz"), 'w') as fl:
+        pickle.dump(pred_dfs, fl, protocol=-1)
+    with bz2.BZ2File(os.path.join(args.use_dir, "out-tune.p.gz"), 'w') as fl:
+        pickle.dump([pars_df, time_df, acc_df, use_clf], fl, protocol=-1)
 
+    cdata.update_split(test_prop=0)
+    train_samps = np.array(cdata.get_train_samples())
     pheno_dict = {mtype: np.array(cdata.train_pheno(mtype))
-                  for mtype in tuple(out_dfs['Infer'].values())[0].index}
+                  for mtype in muts_list}
+
     with bz2.BZ2File(os.path.join(args.use_dir, "out-pheno.p.gz"), 'w') as fl:
         pickle.dump(pheno_dict, fl, protocol=-1)
 
-    auc_df = pd.DataFrame({
-        cis_lbl: dict(zip(infer_df.index, Parallel(
+    auc_vals = pd.DataFrame({
+        cis_lbl: pd.Series(dict(zip(muts_list, Parallel(
             backend='threading', n_jobs=12, pre_dispatch=12)(
-                delayed(calculate_auc)(pheno_dict, infer_vals, mtype)
-                for mtype, infer_vals in infer_df.iterrows()
+                delayed(calculate_auc)(
+                    pheno_dict[mtype],
+                    np.vstack(pred_df.loc[
+                        mtype, train_samps].values).mean(axis=1)
+                    )
+                for mtype in muts_list
                 )
-            ))
-        for cis_lbl, infer_df in out_dfs['Infer'].items()
+            )))
+        for cis_lbl, pred_df in pred_dfs.items()
         })
 
     with bz2.BZ2File(os.path.join(args.use_dir, "out-aucs.p.gz"), 'w') as fl:
-        pickle.dump(auc_df, fl, protocol=-1)
+        pickle.dump(auc_vals, fl, protocol=-1)
 
     random.seed(7609)
     sub_inds = [random.choices([False, True], k=len(cdata.get_samples()))
-                for _ in range(100)]
+                for _ in range(500)]
 
-    conf_df = {
+    conf_dict = {
         cis_lbl: pd.DataFrame.from_records(
-            tuple(zip(cycle(infer_df.index), Parallel(
+            tuple(zip(cycle(muts_list), Parallel(
                 backend='threading', n_jobs=12, pre_dispatch=12)(
-                    delayed(calculate_auc)(pheno_dict, infer_vals, mtype,
-                                           sub_indx)
-                    for sub_indx in sub_inds
-                    for mtype, infer_vals in infer_df.iterrows()
+                    delayed(calculate_auc)(
+                        pheno_dict[mtype][sub_indx],
+                        np.vstack(pred_df.loc[
+                            mtype, train_samps[sub_indx]].values).mean(axis=1)
+                        )
+                    for sub_indx in sub_inds for mtype in muts_list
                     )
                 ))
-            ).pivot_table(index=0, values=1, aggfunc=list)
-        for cis_lbl, infer_df in out_dfs['Infer'].items()
+            ).pivot_table(index=0, values=1, aggfunc=list).iloc[:, 0]
+        for cis_lbl, pred_df in pred_dfs.items()
         }
 
     with bz2.BZ2File(os.path.join(args.use_dir, "out-conf.p.gz"), 'w') as fl:
-        pickle.dump(conf_df, fl, protocol=-1)
+        pickle.dump(conf_dict, fl, protocol=-1)
 
 
 if __name__ == "__main__":
