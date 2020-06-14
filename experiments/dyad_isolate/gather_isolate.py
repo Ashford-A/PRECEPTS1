728a0fe556ff8c18a479890489d3eefc8c86a12e
Michal Grzadkowski <mgrzad@gmail.com>
2020-06-14 16:48:23 -0700

    Recasting dyad isolation consolidation according to the gather+merge paradigm.

--- experiments/dyad_isolate/gather_isolate.py
+++ experiments/dyad_isolate/gather_isolate.py
@@ -19,7 +19,7 @@ from HetMan.experiments.subvariant_isolate import cna_mtypes, ex_mtypes
 from HetMan.experiments.utilities.mutations import ExMcomb
 from HetMan.experiments.subvariant_isolate.merge_isolate import (
     compare_muts, calculate_auc)
-from HetMan.experiments.subgrouping_isolate.merge_isolate import (
+from HetMan.experiments.subgrouping_isolate.gather_isolate import (
     calculate_siml)
 
 from itertools import cycle
@@ -37,6 +37,8 @@ def main():
         "use_dir", type=str,
         help="root folder of where experiment output was saved"
         )
+    parser.add_argument('--task_ids', type=int, nargs='+')
+
     parser.add_argument(
         "--cores", '-c', type=int, default=1,
         help="the number of compute cores available to this script"
@@ -48,38 +50,6 @@ def main():
         )
     args = parser.parse_args()
 
-    # get list of output files from all parallelized jobs
-    file_list = tuple(Path(os.path.join(args.use_dir, 'output')).glob(
-        "out__cv-*_task*.p"))
-    assert (len(file_list) % 40) == 0, "Missing output files detected!"
-
-    # initialize list for storing raw output data
-    task_count = len(file_list) // 40
-    out_data = [[None for task_id in range(task_count)]
-                for cv_id in range(40)]
-
-    # populate raw output list with data from each combination of
-    # parallelized task split and cross-validation run
-    for out_fl in file_list: 
-        base_name = out_fl.stem.split('out__')[1]
-        cv_id = int(base_name.split('cv-')[1].split('_')[0])
-        task_id = int(base_name.split('task-')[1])
-
-        with open(out_fl, 'rb') as f:
-            out_data[cv_id][task_id] = pickle.load(f)
-
-    # find the mutation classifier that was used in this experiment
-    use_clfs = set(out_dict['Clf'] for ols in out_data for out_dict in ols)
-    assert len(use_clfs) == 1, ("Each experiment must be run with "
-                                "exactly one classifier!")
-    use_clf = tuple(use_clfs)[0]
-
-    # find the hyper-parameter tuning grid that was used in this experiment
-    use_tune = set(out_dict['Clf'].tune_priors for ols in out_data
-                   for out_dict in ols)
-    assert len(use_tune) == 1, ("Each experiment must be run with "
-                                "exactly one set of tuning priors!")
-
     # load the -omic datasets for this experiment's tumour cohort
     with bz2.BZ2File(os.path.join(args.use_dir, 'setup',
                                   "cohort-data.p.gz"), 'r') as f:
@@ -89,21 +59,81 @@ def main():
     with open(os.path.join(args.use_dir, 'setup', "muts-list.p"), 'rb') as f:
         muts_list = pickle.load(f)
 
+    # get list of output files from all parallelized jobs
+    file_list = tuple(Path(args.use_dir, 'output').glob("out__cv-*_task*.p"))
+    assert (len(file_list) % 40) == 0, "Missing output files detected!"
+    task_count = len(file_list) // 40
+
+    if args.task_ids is None:
+        use_tasks = set(range(task_count))
+        out_tag = ''
+
+    else:
+        use_tasks = set(args.task_ids)
+        out_tag = "_{}".format('-'.join([
+            str(tsk) for tsk in sorted(use_tasks)]))
+
+    use_muts = [mut for i, mut in enumerate(muts_list)
+                if i % task_count in use_tasks]
+
+    file_sets = {cv_id: set() for cv_id in range(40)}
+    for out_fl in file_list:
+        fl_info = out_fl.stem.split("out__")[1]
+        out_cv = int(fl_info.split("cv-")[1].split("_")[0])
+        out_task = int(fl_info.split("task-")[1])
+
+        if out_task in use_tasks:
+            file_sets[out_cv] |= {out_fl}
+
+    # initialize object that will store raw experiment output data
     out_dfs = {k: {ex_lbl: [None for cv_id in range(40)]
                    for ex_lbl in ['All', 'Iso', 'IsoShal']}
-               for k in ['Pars', 'Time', 'Acc', 'Pred']}
+               for k in ['Pars', 'Time', 'Acc']}
+    out_clf = None
+    out_tune = None
 
-    pred_lists = {ex_lbl: [None for cv_id in range(40)]
-                  for ex_lbl in ['All', 'Iso', 'IsoShal']}
+    pred_lists = {
+        ex_lbl: [
+            pd.DataFrame(index=use_muts,
+                         columns=cdata.get_samples()).applymap(lambda x: [])
+            for cv_id in range(40)
+            ]
+        for ex_lbl in ['All', 'Iso', 'IsoShal']
+        }
 
-    for cv_id, ols in enumerate(out_data):
-        for k in out_dfs:
-            for ex_lbl in ['All', 'Iso', 'IsoShal']:
-                out_dfs[k][ex_lbl][cv_id] = pd.DataFrame({
-                    mtype: out_vals[ex_lbl]
-                    for out_dict in ols
-                    for mtype, out_vals in out_dict[k].items()
-                    }).transpose()
+    mut_samps = {mut: {samp for samp, phn in zip(cdata.get_train_samples(),
+                                                 cdata.train_pheno(mut))
+                       if phn}
+                 for mut in use_muts}
+
+    mut_genes = {mut: mut.get_labels() for mut in use_muts}
+    gene_samps = {gene: mtree.get_samples() for gene, mtree in use_mtree}
+    shal_samps = {gene: dict(cna_mtypes)['Shal'].get_samples(mtree)
+                  for gene, mtree in use_mtree}
+
+    for cv_id, out_fls in file_sets.items():
+        out_list = []
+
+        for out_fl in out_fls:
+            with open(out_fl, 'rb') as f:
+                out_list += [pickle.load(f)]
+
+        for out_dicts in out_list:
+            if out_clf is None:
+                out_clf = out_dicts['Clf']
+
+            else:
+                assert out_clf == out_dicts['Clf'], (
+                    "Each experiment must be run with the same classifier!")
+
+            if out_tune is None:
+                out_tune = out_dicts['Clf'].tune_priors
+
+            else:
+                assert out_tune == out_dicts['Clf'].tune_priors, (
+                    "Each experiment must be run with exactly "
+                    "one set of tuning priors!"
+                    )
 
         cdata_samps = sorted(cdata.get_samples())
         random.seed((cv_id // 4) * 3901 + 23)
@@ -116,63 +146,68 @@ def main():
                       'test': cdata.get_test_samples()}
 
         for ex_lbl in ['All', 'Iso', 'IsoShal']:
-            use_muts = out_dfs['Pred'][ex_lbl][cv_id].index
-            assert sorted(use_muts) == sorted(muts_list), (
+            out_preds = pd.DataFrame({
+                mut: out_vals[ex_lbl] for out_dicts in out_list
+                for mut, out_vals in out_dicts['Pred'].items()
+                }).transpose()
+
+            assert sorted(out_preds.index) == sorted(use_muts), (
                 "Mutations with `{}` predictions for c-v fold <{}> don't "
                 "match those enumerated during setup!".format(ex_lbl, cv_id)
                 )
 
-            pred_lists[ex_lbl][cv_id] = pd.DataFrame(
-                index=use_muts, columns=cdata.get_samples()).applymap(
-                    lambda x: [])
-
             pred_lists[ex_lbl][cv_id][samps_dict['test']] = pd.DataFrame(
-                [preds for preds in out_dfs['Pred'][ex_lbl][cv_id].test],
-                index=use_muts, columns=samps_dict['test']
+                out_preds.test.values.tolist(),
+                index=out_preds.index, columns=samps_dict['test']
                 ).applymap(lambda x: [x])
 
-            if 'train' in out_dfs['Pred'][ex_lbl][cv_id]:
-                train_mat = out_dfs['Pred'][ex_lbl][cv_id].train[
-                    ~out_dfs['Pred'][ex_lbl][cv_id].train.isnull()]
+            if 'train' in out_preds:
+                train_mat = out_preds.train[~out_preds.train.isnull()]
 
-                for mtype, train_preds in train_mat.iteritems():
-                    cur_genes = mtype.get_labels()
+                for mut, train_preds in train_mat.iteritems():
+                    use_samps = reduce(or_, [gene_samps[gn]
+                                             for gn in mut_genes[mut]]).copy()
+                    use_samps -= mut_samps[mut]
 
-                    mut_samps = reduce(or_,
-                                       [use_mtree[gene].get_samples()
-                                        for gene in cur_genes])
-
-                    shal_samps = reduce(
-                        or_,
-                        [dict(cna_mtypes)['Shal'].get_samples(use_mtree[gene])
-                         for gene in cur_genes]
-                        )
-
-                    mtype_samps = {
-                        samp for samp, phn in zip(samps_dict['train'],
-                                                  cdata.train_pheno(mtype))
-                        if phn
-                        }
-
-                    if ex_lbl == 'Iso':
-                        use_samps = mut_samps - mtype_samps
-                    elif ex_lbl == 'IsoShal':
-                        use_samps = mut_samps - mtype_samps - shal_samps
+                    if ex_lbl == 'IsoShal':
+                        use_samps -= reduce(or_, [shal_samps[gn]
+                                                  for gn in mut_genes[mut]])
 
                     out_samps = sorted(use_samps & set(samps_dict['train']))
-                    pred_lists[ex_lbl][cv_id].loc[mtype][out_samps] = [
+                    pred_lists[ex_lbl][cv_id].loc[mut][out_samps] = [
                         [x] for x in train_preds]
+
+        for k in out_dfs:
+            for ex_lbl in ['All', 'Iso', 'IsoShal']:
+                out_dfs[k][ex_lbl][cv_id] = pd.DataFrame({
+                    mut: out_vals[ex_lbl] for out_dicts in out_list
+                    for mut, out_vals in out_dicts[k].items()
+                    }).transpose()
 
     pred_dfs = {ex_lbl: reduce(add, pred_mats)
                 for ex_lbl, pred_mats in pred_lists.items()}
+
     assert (pred_dfs['All'].applymap(len) == 10).values.all(), (
         "Incorrect number of testing CV scores!")
+
+    for mut in pred_dfs['All'].index:
+        hld_samps = reduce(or_, [gene_samps[gn]
+                                 for gn in mut_genes[mut]]).copy()
+
+        hld_samps -= mut_samps[mut]
+        assert (pred_dfs['Iso'].loc[mut, hld_samps].apply(len) == 40).all(), (
+            "Incorrect number of testing CV scores!")
+
+        hld_samps -= reduce(or_, [shal_samps[gn] for gn in mut_genes[mut]])
+        assert (pred_dfs['IsoShal'].loc[
+            mut, hld_samps].apply(len) == 40).all(), (
+                "Incorrect number of testing CV scores!")
 
     pars_dfs = {ex_lbl: pd.concat(out_dfs['Pars'][ex_lbl], axis=1)
                 for ex_lbl in ['All', 'Iso', 'IsoShal']}
 
     for pars_df in pars_dfs.values():
-        assert pars_df.shape[1] == (40 * len(use_clf.tune_priors)), (
+        assert pars_df.shape[1] == (40 * len(out_clf.tune_priors)), (
             "Tuned parameter values missing for some CVs!")
 
     time_dfs = {ex_lbl: pd.concat(out_dfs['Time'][ex_lbl], axis=1)
@@ -181,7 +216,7 @@ def main():
     for time_df in time_dfs.values():
         assert time_df.shape[1] == 80, (
             "Algorithm fitting times missing for some CVs!")
-        assert (time_df.applymap(len) == use_clf.test_count).values.all(), (
+        assert (time_df.applymap(len) == out_clf.test_count).values.all(), (
             "Algorithm fit times missing for some hyper-parameter values!")
 
     acc_dfs = {ex_lbl: pd.concat(out_dfs['Acc'][ex_lbl], axis=1)
@@ -190,58 +225,59 @@ def main():
     for acc_df in acc_dfs.values():
         assert acc_df.shape[1] == 120, (
             "Algorithm tuning accuracies missing for some CVs!")
-        assert (acc_df.applymap(len) == use_clf.test_count).values.all(), (
+        assert (acc_df.applymap(len) == out_clf.test_count).values.all(), (
             "Algorithm tuning stats missing for some hyper-parameter values!")
 
     for out_dfs in [pred_dfs, pars_dfs, time_dfs, acc_dfs]:
         for out_df in out_dfs.values():
-            assert compare_muts(out_df.index, muts_list), (
+            assert compare_muts(out_df.index, use_muts), (
                 "Mutations for which predictions were made do not match "
                 "the list of mutations enumerated during setup!"
                 )
 
     if not args.test:
-        with bz2.BZ2File(os.path.join(args.use_dir, "out-pred.p.gz"),
+        with bz2.BZ2File(os.path.join(args.use_dir, 'merge',
+                                      "out-pred{}.p.gz".format(out_tag)),
                          'w') as fl:
             pickle.dump(pred_dfs, fl, protocol=-1)
 
-        with bz2.BZ2File(os.path.join(args.use_dir, "out-tune.p.gz"),
+        with bz2.BZ2File(os.path.join(args.use_dir, 'merge',
+                                      "out-tune{}.p.gz".format(out_tag)),
                          'w') as fl:
-            pickle.dump([pars_dfs, time_dfs, acc_dfs, use_clf],
+            pickle.dump([pars_dfs, time_dfs, acc_dfs, out_clf],
                         fl, protocol=-1)
 
     cdata.update_split(test_prop=0)
     train_samps = np.array(cdata.get_train_samples())
-    pheno_dict = {mtype: np.array(cdata.train_pheno(mtype))
-                  for mtype in muts_list}
+    pheno_dict = {mut: np.array(cdata.train_pheno(mut)) for mut in use_muts}
 
     if not args.test:
-        with bz2.BZ2File(os.path.join(args.use_dir, "out-pheno.p.gz"),
+        with bz2.BZ2File(os.path.join(args.use_dir, 'merge',
+                                      "out-pheno{}.p.gz".format(out_tag)),
                          'w') as fl:
             pickle.dump(pheno_dict, fl, protocol=-1)
 
     auc_dicts = {
         ex_lbl: {
             'CV': pd.DataFrame.from_records(
-                tuple(zip(cycle(muts_list), Parallel(
-                    backend='threading',
-                    n_jobs=args.cores, pre_dispatch=args.cores
+                tuple(zip(cycle(use_muts), Parallel(
+                    prefer='threads',
+                    n_jobs=args.cores, pre_dispatch=args.cores * 10
                     )(delayed(calculate_auc)(
-                        pheno_dict[mtype],
-                        pred_dfs[ex_lbl].loc[mtype, train_samps],
+                        pheno_dict[mut],
+                        pred_dfs[ex_lbl].loc[mut][train_samps],
                         cv_indx=cv_id
-                        ) for cv_id in range(10) for mtype in muts_list)
+                        ) for cv_id in range(10) for mut in use_muts)
                     ))
                 ).pivot_table(index=0, values=1, aggfunc=list).iloc[:, 0],
 
-            'mean': pd.Series(dict(zip(muts_list, Parallel(
-                backend='threading',
-                n_jobs=args.cores, pre_dispatch=args.cores
+            'mean': pd.Series(dict(zip(use_muts, Parallel(
+                prefer='threads',
+                n_jobs=args.cores, pre_dispatch=args.cores * 10
                 )(delayed(calculate_auc)(
-                    pheno_dict[mtype],
-                    pred_dfs[ex_lbl].loc[mtype, train_samps],
+                    pheno_dict[mut], pred_dfs[ex_lbl].loc[mut][train_samps],
                     use_mean=True
-                    ) for mtype in muts_list)
+                    ) for mut in use_muts)
                 )))
             }
         for ex_lbl in ['All', 'Iso', 'IsoShal']
@@ -252,7 +288,8 @@ def main():
         auc_dicts[ex_lbl]['CV'].index.name = None
 
     if not args.test:
-        with bz2.BZ2File(os.path.join(args.use_dir, "out-aucs.p.gz"),
+        with bz2.BZ2File(os.path.join(args.use_dir, 'merge',
+                                      "out-aucs{}.p.gz".format(out_tag)),
                          'w') as fl:
             pickle.dump(auc_dicts, fl, protocol=-1)
 
@@ -263,14 +300,14 @@ def main():
     conf_lists = {
         ex_lbl: {
             'mean': pd.DataFrame.from_records(
-                tuple(zip(cycle(muts_list), Parallel(
-                    backend='threading',
-                    n_jobs=args.cores, pre_dispatch=args.cores
+                tuple(zip(cycle(use_muts), Parallel(
+                    prefer='threads',
+                    n_jobs=args.cores, pre_dispatch=args.cores * 10
                     )(delayed(calculate_auc)(
-                        pheno_dict[mtype][sub_indx],
-                        pred_dfs[ex_lbl].loc[mtype, train_samps[sub_indx]],
+                        pheno_dict[mut][sub_indx],
+                        pred_dfs[ex_lbl].loc[mut][train_samps[sub_indx]],
                         use_mean=True
-                        ) for sub_indx in sub_inds for mtype in muts_list)
+                        ) for sub_indx in sub_inds for mut in use_muts)
                     ))
                 ).pivot_table(index=0, values=1, aggfunc=list).iloc[:, 0]
             }
@@ -282,16 +319,17 @@ def main():
         conf_lists[ex_lbl]['mean'].index.name = None
 
     if not args.test:
-        with bz2.BZ2File(os.path.join(args.use_dir, "out-conf.p.gz"),
+        with bz2.BZ2File(os.path.join(args.use_dir, 'merge',
+                                      "out-conf{}.p.gz".format(out_tag)),
                          'w') as fl:
             pickle.dump(conf_lists, fl, protocol=-1)
 
     mcomb_lists = {
-        'Iso': {mut for mut in muts_list
+        'Iso': {mut for mut in use_muts
                 if (isinstance(mut, ExMcomb)
                     and not (mut.all_mtype
                              & dict(cna_mtypes)['Shal']).is_empty())},
-        'IsoShal': {mut for mut in muts_list
+        'IsoShal': {mut for mut in use_muts
                     if (isinstance(mut, ExMcomb)
                         and (mut.all_mtype
                              & dict(cna_mtypes)['Shal']).is_empty())}
@@ -299,35 +337,35 @@ def main():
 
     for ex_lbl, ex_mtype in ex_mtypes:
         for mcomb in mcomb_lists[ex_lbl]:
-            cur_genes = mcomb.get_labels()
-            gene_ex = reduce(or_, [MuType({('Gene', cur_gene): ex_mtype})
-                                   for cur_gene in cur_genes])
+            gene_ex = reduce(or_, [MuType({('Gene', gn): ex_mtype})
+                                   for gn in mut_genes[mcomb]])
 
             all_mtype = reduce(
                 or_,
-                [MuType({('Gene', cur_gene): use_mtree[cur_gene].allkey()})
-                 for cur_gene in cur_genes]
+                [MuType({('Gene', gn): use_mtree[gn].allkey()})
+                 for gn in mut_genes[mcomb]]
                 )
 
             pheno_dict[ex_lbl, mcomb] = np.array(cdata.train_pheno(
                 all_mtype - gene_ex))
 
-    siml_lists = {
+    siml_dicts = {
         ex_lbl: dict(zip(mcomb_lists[ex_lbl], Parallel(
-            backend='threading',
-            n_jobs=args.cores, pre_dispatch=args.cores
+            prefer='threads',
+            n_jobs=args.cores, pre_dispatch=args.cores * 10
             )(delayed(calculate_siml)(
                 mcomb, pheno_dict, (ex_lbl, mcomb),
-                pred_dfs[ex_lbl].loc[mcomb, train_samps]
+                pred_dfs[ex_lbl].loc[mcomb][train_samps]
                 ) for mcomb in mcomb_lists[ex_lbl])
             ))
         for ex_lbl in ['Iso', 'IsoShal']
         }
 
     if not args.test:
-        with bz2.BZ2File(os.path.join(args.use_dir, "out-siml.p.gz"),
+        with bz2.BZ2File(os.path.join(args.use_dir, 'merge',
+                                      "out-siml{}.p.gz".format(out_tag)),
                          'w') as fl:
-            pickle.dump(siml_lists, fl, protocol=-1)
+            pickle.dump(siml_dicts, fl, protocol=-1)
 
 
 if __name__ == "__main__":
