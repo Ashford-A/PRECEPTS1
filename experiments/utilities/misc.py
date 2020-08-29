950004c87b6a3ed9b01066d3e26fb46be19a5417
Michal Grzadkowski <mgrzad@gmail.com>
2020-08-29 16:28:25 -0700

    Updating gene-specific isolation infrastructure.

--- experiments/utilities/misc.py
+++ experiments/utilities/misc.py
@@ -4,6 +4,11 @@ from colorsys import hls_to_rgb
 from importlib import import_module
 
 
+def compare_muts(*muts_lists):
+    return len(set(tuple(sorted(muts_list))
+                   for muts_list in muts_lists)) == 1
+
+
 def get_distr_transform(tune_distr):
     distr_diff = np.array(tune_distr[1:]) - np.array(tune_distr[:-1])
     diff_min = np.log10(np.min(distr_diff))
@@ -20,11 +25,6 @@ def get_distr_transform(tune_distr):
 def warning_on_one_line(message, category, filename, lineno,
                         file=None, line=None):
     return '%s:%s: %s: %s\n' % (filename, lineno, category.__name__, message)
-
-
-def ordinal_label(n):
-    return "%d%s" % (n, {1: "st", 2: "nd", 3: "rd"}.get(n if n < 20
-                                                        else n % 10, "th"))
 
 
 def choose_label_colour(gene, clr_seed=15707, clr_lum=0.5, clr_sat=0.8):
