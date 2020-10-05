bf083ab6e99c4e5cb8baeda26256d12476d19216
Michal Grzadkowski <mgrzad@gmail.com>
2020-10-05 16:26:18 -0700

    Updating sub-sampled AUC plots in subgrouping testing experiment.

--- experiments/subgrouping_test/plot_conf.py
+++ experiments/subgrouping_test/plot_conf.py
@@ -1,19 +1,14 @@
 
-import os
-import sys
-
-base_dir = os.path.join(os.environ['DATADIR'], 'HetMan', 'subvariant_test')
-sys.path.extend([os.path.join(os.path.dirname(__file__), '..', '..', '..')])
-plot_dir = os.path.join(base_dir, 'plots', 'conf')
-
-from HetMan.experiments.subvariant_test import pnt_mtype, copy_mtype
-from HetMan.experiments.subvariant_tour.utils import RandomType
-from HetMan.experiments.subvariant_test.utils import (
-    get_fancy_label, choose_label_colour)
-from HetMan.experiments.subvariant_infer import variant_clrs
-from HetMan.experiments.subvariant_tour.plot_aucs import place_labels
+from ..utilities.mutations import pnt_mtype, copy_mtype, RandomType
 from dryadic.features.mutations import MuType
 
+from ..subgrouping_test import base_dir
+from ..utilities.misc import choose_label_colour
+from ..utilities.colour_maps import variant_clrs
+from ..utilities.labels import get_fancy_label
+from ..utilities.label_placement import place_scatter_labels
+
+import os
 import argparse
 from pathlib import Path
 import bz2
@@ -23,15 +18,16 @@ import numpy as np
 import pandas as pd
 
 import matplotlib as mpl
-mpl.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
+mpl.use('Agg')
 plt.style.use('fivethirtyeight')
-plt.rcParams['axes.facecolor']='white'
-plt.rcParams['savefig.facecolor']='white'
-plt.rcParams['axes.edgecolor']='white'
+plt.rcParams['axes.facecolor'] = 'white'
+plt.rcParams['savefig.facecolor'] = 'white'
+plt.rcParams['axes.edgecolor'] = 'white'
+plot_dir = os.path.join(base_dir, 'plots', 'conf')
 
 
 def plot_auc_comparison(auc_vals, conf_vals, pheno_dict, args):
@@ -40,10 +36,8 @@ def plot_auc_comparison(auc_vals, conf_vals, pheno_dict, args):
     conf_df = pd.DataFrame.from_dict(dict(zip(
         conf_vals.index, conf_vals.apply(
             lambda vals: np.percentile(vals, q=[0, 25, 50, 75, 100])).values
-        )), orient='index')
-
+        )), orient='index', columns=['Min', '1Q', 'Med', '3Q', 'Max'])
     assert set(auc_vals.index) == set(conf_df.index)
-    conf_df.columns = ['Min', '1Q', 'Med', '3Q', 'Max']
 
     for mtype in auc_vals.index:
         if isinstance(mtype, RandomType):
@@ -90,23 +84,21 @@ def plot_distr_comparisons(auc_vals, conf_vals, pheno_dict, args):
 
     conf_list = conf_vals[[
         not isinstance(mtype, RandomType)
-        and (mtype.subtype_list()[0][1] & copy_mtype).is_empty()
-        for mtype in auc_vals.index
+        and (tuple(mtype.subtype_iter())[0][1] & copy_mtype).is_empty()
+        for mtype in conf_vals.index
         ]]
 
     for gene, conf_vec in conf_list.apply(
             lambda confs: np.percentile(confs, 25)).groupby(
-                lambda mtype: mtype.get_labels()[0]):
-
+                lambda mtype: tuple(mtype.label_iter())[0]):
         if len(conf_vec) > 1:
             base_mtype = MuType({('Gene', gene): pnt_mtype})
-            base_indx = conf_vec.index.get_loc(base_mtype)
 
+            base_indx = conf_vec.index.get_loc(base_mtype)
             best_subtype = conf_vec[:base_indx].append(
                 conf_vec[(base_indx + 1):]).idxmax()
-            best_indx = conf_vec.index.get_loc(best_subtype)
 
-            if conf_vec[best_indx] > 0.7:
+            if conf_vec[best_subtype] > 0.7:
                 gene_dict[gene] = (
                     choose_label_colour(gene), base_mtype, best_subtype,
                     np.greater.outer(conf_list[best_subtype],
@@ -134,9 +126,9 @@ def plot_distr_comparisons(auc_vals, conf_vals, pheno_dict, args):
                        cut=0, linewidth=0, width=0.93)
 
         axarr[i].scatter(0, auc_vals[best_subtype], 
-                         s=41, c=[gene_clr], edgecolor='0.23', alpha=0.97)
+                         s=47, c=[gene_clr], edgecolor='0.31', alpha=0.93)
         axarr[i].scatter(1, auc_vals[base_mtype],
-                         s=41, c=[gene_clr], edgecolor='0.23', alpha=0.53)
+                         s=47, c=[gene_clr], edgecolor='0.31', alpha=0.41)
 
         axarr[i].get_children()[0].set_alpha(0.71)
         axarr[i].get_children()[2].set_alpha(0.29)
@@ -166,8 +158,11 @@ def plot_distr_comparisons(auc_vals, conf_vals, pheno_dict, args):
         else:
             axarr[i].set_ylabel('')
 
-    if 0.47 < ymin < 0.51:
-        ymin = 0.445
+    fig.text(plt_size ** 0.71 / 97, 1 / 19, "conf.\nscore",
+             fontsize=15, weight='semibold', ha='right', va='bottom')
+
+    if 0.463 < ymin < 0.513:
+        ymin = 0.453
     for ax in axarr:
         ax.set_ylim([ymin, 1 + (1 - ymin) / 23])
 
@@ -181,101 +176,86 @@ def plot_distr_comparisons(auc_vals, conf_vals, pheno_dict, args):
 
 
 def plot_sub_comparisons(conf_vals, pheno_dict, args):
-    fig, ax = plt.subplots(figsize=(11, 11))
+    fig, ax = plt.subplots(figsize=(10.3, 11))
 
-    pnt_dict = dict()
+    plot_dict = dict()
     clr_dict = dict()
+    plt_min = 0.57
 
     conf_list = conf_vals[[
         not isinstance(mtype, RandomType)
-        and (mtype.subtype_list()[0][1] & copy_mtype).is_empty()
+        and (tuple(mtype.subtype_iter())[0][1] & copy_mtype).is_empty()
         for mtype in conf_vals.index
-        ]]
+        ]].apply(lambda confs: np.percentile(confs, 25))
 
-    conf_list = conf_list.apply(lambda confs: np.percentile(confs, 25))
     for gene, conf_vec in conf_list.groupby(
-            lambda mtype: mtype.get_labels()[0]):
-
+            lambda mtype: tuple(mtype.label_iter())[0]):
         if len(conf_vec) > 1:
             base_mtype = MuType({('Gene', gene): pnt_mtype})
-            base_indx = conf_vec.index.get_loc(base_mtype)
 
+            base_indx = conf_vec.index.get_loc(base_mtype)
             best_subtype = conf_vec[:base_indx].append(
                 conf_vec[(base_indx + 1):]).idxmax()
-            best_indx = conf_vec.index.get_loc(best_subtype)
 
-            if conf_vec[best_indx] > 0.6:
-                clr_dict[gene] = choose_label_colour(gene)
+            if conf_vec[best_subtype] > 0.6:
+                auc_tupl = conf_vec[base_mtype], conf_vec[best_subtype]
+                clr_dict[auc_tupl] = choose_label_colour(gene)
+
                 base_size = np.mean(pheno_dict[base_mtype])
-                best_prop = np.mean(pheno_dict[best_subtype]) / base_size
+                plt_size = 0.07 * base_size ** 0.5
+                plot_dict[auc_tupl] = [plt_size, ('', '')]
+                plt_min = min(plt_min, conf_vec[base_indx] - 0.053,
+                              conf_vec[best_subtype] - 0.029)
 
-                conf_sc = np.greater.outer(conf_list[best_subtype],
-                                           conf_list[base_mtype]).mean()
+                best_prop = np.mean(pheno_dict[best_subtype]) / base_size
+                conf_sc = np.greater.outer(conf_vals[best_subtype],
+                                           conf_vals[base_mtype]).mean()
 
                 if conf_sc > 0.8:
-                    mtype_lbl = '\n'.join(
-                        get_fancy_label(best_subtype).split('\n')[1:])
+                    plot_dict[auc_tupl][1] = gene, get_fancy_label(
+                        tuple(best_subtype.subtype_iter())[0][1],
+                        pnt_link='\n', phrase_link=' '
+                        )
 
-                    pnt_dict[conf_vec[base_indx], conf_vec[best_indx]] = (
-                        base_size ** 0.53, (gene, mtype_lbl))
+                elif auc_tupl[0] > 0.7 or auc_tupl[1] > 0.7:
+                    plot_dict[auc_tupl][1] = gene, ''
 
-                elif conf_vec[base_indx] > 0.7 or conf_vec[best_indx] > 0.7:
-                    pnt_dict[conf_vec[base_indx], conf_vec[best_indx]] = (
-                        base_size ** 0.53, (gene, ''))
-
-                else:
-                    pnt_dict[conf_vec[base_indx], conf_vec[best_indx]] = (
-                        base_size ** 0.53, ('', ''))
+                auc_bbox = (auc_tupl[0] - plt_size / 2,
+                            auc_tupl[1] - plt_size / 2, plt_size, plt_size)
 
                 pie_ax = inset_axes(
-                    ax, width=base_size ** 0.5, height=base_size ** 0.5,
-                    bbox_to_anchor=(conf_vec[base_indx], conf_vec[best_indx]),
-                    bbox_transform=ax.transData, loc=10,
+                    ax, width='100%', height='100%',
+                    bbox_to_anchor=auc_bbox, bbox_transform=ax.transData,
                     axes_kwargs=dict(aspect='equal'), borderpad=0
                     )
 
-                pie_ax.pie(x=[best_prop, 1 - best_prop], explode=[0.29, 0],
-                           colors=[clr_dict[gene] + (0.77,),
-                                   clr_dict[gene] + (0.29,)])
+                pie_ax.pie(x=[best_prop, 1 - best_prop],
+                           colors=[clr_dict[auc_tupl] + (0.77,),
+                                   clr_dict[auc_tupl] + (0.29,)],
+                           explode=[0.29, 0], startangle=90)
 
-    lbl_pos = place_labels(pnt_dict)
-    for (pnt_x, pnt_y), pos in lbl_pos.items():
-        ax.text(pos[0][0], pos[0][1] + 700 ** -1,
-                pnt_dict[pnt_x, pnt_y][1][0],
-                size=13, ha=pos[1], va='bottom')
-        ax.text(pos[0][0], pos[0][1] - 700 ** -1,
-                pnt_dict[pnt_x, pnt_y][1][1],
-                size=9, ha=pos[1], va='top')
+    plt_lims = plt_min, 1 + (1 - plt_min) / 61
+    ax.plot(plt_lims, [0.5, 0.5],
+            color='black', linewidth=1.3, linestyle=':', alpha=0.71)
+    ax.plot([0.5, 0.5], plt_lims,
+            color='black', linewidth=1.3, linestyle=':', alpha=0.71)
 
-        x_delta = pnt_x - pos[0][0]
-        y_delta = pnt_y - pos[0][1]
-        ln_lngth = np.sqrt((x_delta ** 2) + (y_delta ** 2))
-
-        # if the label is sufficiently far away from its point...
-        if ln_lngth > (0.021 + pnt_dict[pnt_x, pnt_y][0] / 31):
-            use_clr = clr_dict[pnt_dict[pnt_x, pnt_y][1][0]]
-            pnt_gap = pnt_dict[pnt_x, pnt_y][0] / (29 * ln_lngth)
-            lbl_gap = 0.006 / ln_lngth
-
-            ax.plot([pnt_x - pnt_gap * x_delta,
-                     pos[0][0] + lbl_gap * x_delta],
-                    [pnt_y - pnt_gap * y_delta,
-                     pos[0][1] + lbl_gap * y_delta
-                     + 0.008 + 0.004 * np.sign(y_delta)],
-                    c=use_clr, linewidth=2.3, alpha=0.27)
-
-    ax.plot([0.48, 1.0005], [1, 1], color='black', linewidth=1.9, alpha=0.89)
-    ax.plot([1, 1], [0.48, 1.0005], color='black', linewidth=1.9, alpha=0.89)
-    ax.plot([0.49, 0.997], [0.49, 0.997],
-            linewidth=2.1, linestyle='--', color='#550000', alpha=0.41)
-
-    ax.set_xlim([0.48, 1.01])
-    ax.set_ylim([0.48, 1.01])
+    ax.plot(plt_lims, [1, 1], color='black', linewidth=1.9, alpha=0.89)
+    ax.plot([1, 1], plt_lims, color='black', linewidth=1.9, alpha=0.89)
+    ax.plot(plt_lims, plt_lims,
+            color='#550000', linewidth=2.1, linestyle='--', alpha=0.41)
 
     ax.set_xlabel("1st quartile of down-sampled AUCs"
                   "\nusing all point mutations", size=21, weight='semibold')
     ax.set_ylabel("1st quartile of down-sampled AUCs"
                   "\nof best found subgrouping", size=21, weight='semibold')
+
+    if plot_dict:
+        lbl_pos = place_scatter_labels(plot_dict, clr_dict, fig, ax,
+                                       plt_lims=[plt_lims, plt_lims])
+
+    ax.set_xlim(plt_lims)
+    ax.set_ylim(plt_lims)
 
     plt.savefig(
         os.path.join(plot_dir, '__'.join([args.expr_source, args.cohort]),
@@ -288,38 +268,36 @@ def plot_sub_comparisons(conf_vals, pheno_dict, args):
 
 def main():
     parser = argparse.ArgumentParser(
-        "Plots the down-sampled AUCs for a particular classifier on the "
-        "mutations enumerated for a given cohort."
+        'plot_conf',
+        description="Plots comparisons of down-sampled AUCs in a cohort."
         )
 
-    parser.add_argument('expr_source',
-                        help="a source of expression data", type=str)
-    parser.add_argument('cohort', help="a TCGA cohort", type=str)
-    parser.add_argument('classif', help="a mutation classifier", type=str)
+    parser.add_argument('expr_source', help="a source of expression datasets")
+    parser.add_argument('cohort', help="a tumour cohort")
+    parser.add_argument('classif', help="a mutation classifier")
 
     args = parser.parse_args()
     out_datas = [
-        out_file.parts[-2:] for out_file in Path(base_dir).glob(
-            "{}__{}__samps-*/out-conf__*__{}.p.gz".format(
-                args.expr_source, args.cohort, args.classif)
-            )
+        out_file.parts[-2:] for out_file in Path(base_dir).glob(os.path.join(
+            "{}__{}__samps-*".format(args.expr_source, args.cohort),
+            "out-trnsf__*__{}.p.gz".format(args.classif)
+            ))
         ]
 
     out_list = pd.DataFrame([{'Samps': int(out_data[0].split('__samps-')[1]),
                               'Levels': '__'.join(out_data[1].split(
-                                  'out-conf__')[1].split('__')[:-1])}
+                                  'out-trnsf__')[1].split('__')[:-1])}
                              for out_data in out_datas])
 
     if out_list.shape[0] == 0:
         raise ValueError("No experiment output found for these parameters!")
 
     out_use = out_list.groupby('Levels')['Samps'].min()
-    if 'Exon__Location__Protein' not in out_use.index:
+    if 'Consequence__Exon' not in out_use.index:
         raise ValueError("Cannot compare AUCs until this experiment is run "
-                         "with mutation levels `Exon__Location__Protein` "
+                         "with mutation levels `Consequence__Exon` "
                          "which tests genes' base mutations!")
 
-    # create directory where plots will be stored
     os.makedirs(os.path.join(plot_dir,
                              '__'.join([args.expr_source, args.cohort])),
                 exist_ok=True)
@@ -348,7 +326,7 @@ def main():
                                       "out-conf__{}__{}.p.gz".format(
                                           lvls, args.classif)),
                          'r') as f:
-            conf_dict[lvls] = pickle.load(f)['mean']
+            conf_dict[lvls] = pickle.load(f)
 
     auc_vals = pd.concat(auc_dict.values())
     conf_vals = pd.concat(conf_dict.values())
