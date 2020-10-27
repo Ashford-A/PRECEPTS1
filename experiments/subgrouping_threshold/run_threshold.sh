a0952081ab20edf59230654a9ac34faa97c91a89
Michal Grzadkowski <mgrzad@gmail.com>
2020-10-27 10:41:14 -0700

    Updating subgrouping threshold experiment infrastructure.

--- experiments/subgrouping_threshold/run_threshold.sh
+++ experiments/subgrouping_threshold/run_threshold.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
-#SBATCH --job-name=subv-thresh
+#SBATCH --job-name=subg-thresh
 #SBATCH --verbose
 
-
-source activate HetMan
+start_time=$( date +%s )
+source activate research
 rewrite=false
+count_only=false
 
 # collect command line arguments
-while getopts t:g:c:m:r var
+while getopts :e:t:s:l:c:m:rn var
 do
 	case "$var" in
-		t)	cohort=$OPTARG;;
-		c)	classif=$OPTARG;;
-		m)	test_max=$OPTARG;;
-		r)      rewrite=true;;
-		[?])    echo "Usage: $0 [-t] tumour cohort " \
-				"[-c] mutation classifier [-m] maximum tests per node" \
-				"[-r] rewrite existing results?"
+		t)  cohort=$OPTARG;;
+		c)  classif=$OPTARG;;
+		m)  test_max=$OPTARG;;
+		r)  rewrite=true;;
+		n)  count_only=true;;
+		[?])  echo "Usage: $0 " \
+				"[-t] tumour cohort" \
+				"[-c] mutation classifier" \
+				"[-m] maximum number of tests per node" \
+				"[-r] rewrite existing results?" \
+				"[-n] only enumerate, don't classify?"
 			exit 1;;
 	esac
 done
 
 # decide where intermediate files will be stored, find code source directory and input files
-OUTDIR=$TEMPDIR/HetMan/subvariant_threshold/$cohort/$classif
-FINALDIR=$DATADIR/HetMan/subvariant_threshold
-export RUNDIR=$CODEDIR/HetMan/experiments/subvariant_threshold
-source $RUNDIR/files.sh
+OUTDIR=$TEMPDIR/dryads-research/subgrouping_threshold/$cohort/$classif
+FINALDIR=$DATADIR/dryads-research/subgrouping_threshold
+export RUNDIR=$CODEDIR/dryads-research/experiments/subgrouping_threshold
 
 # if we want to rewrite the experiment, remove the intermediate output directory
 if $rewrite
@@ -34,44 +38,79 @@ then
 fi
 
 # create the directories where intermediate and final output will be stored, move to working directory
-mkdir -p $FINALDIR $TEMPDIR/HetMan/subvariant_threshold/setup
-mkdir -p $OUTDIR/setup $OUTDIR/output $OUTDIR/slurm
-cd $OUTDIR
+mkdir -p $TEMPDIR/dryads-research/subgrouping_threshold/setup
+mkdir -p $FINALDIR $OUTDIR/setup $OUTDIR/output $OUTDIR/slurm
+cd $OUTDIR || exit
 
-# initiate version control in this directory if it hasn't been already
-if [ ! -d .dvc ]
+rm -rf .snakemake
+dvc init --no-scm -f
+export PYTHONPATH="$CODEDIR"
+
+eval "$( python -m dryads-research.experiments.utilities.data_dirs $cohort )"
+
+if $rewrite
 then
-	dvc init --no-scm
+	# enumerate the mutation types that will be tested in this experiment
+	dvc run -d $COH_DIR -d $GENCODE_DIR -d $ONCOGENE_LIST \
+		-d $SUBTYPE_LIST -d $RUNDIR/setup_threshold.py \
+		-d $CODEDIR/dryads-research/environment.yml \
+		-o setup/muts-list.p -m setup/muts-count.txt \
+		-f setup.dvc --overwrite-dvcfile \
+		python -m dryads-research.experiments.subgrouping_threshold.setup_threshold \
+		$cohort $classif $OUTDIR $DATADIR/dryads-research
 fi
 
-# enumerate the mutation types that will be tested in this experiment
-dvc run -d $firehose_dir -d $mc3_file -d $gencode_file -d $subtype_file \
-	-d $RUNDIR/setup_threshold.py -d $CODEDIR/HetMan/environment.yml \
-	-o setup/muts-list.p -o setup/feat-list.p \
-	-m setup/muts-count.txt -f setup.dvc --overwrite-dvcfile \
-	python $RUNDIR/setup_threshold.py $cohort $classif \
-	$OUTDIR $DATADIR/HetMan
-
-# calculate how many parallel tasks the mutations will be tested over
-muts_count=$(cat setup/muts-count.txt)
-task_count=$(( $(( $muts_count - 1 )) / $test_max + 1 ))
-
-# remove the Snakemake locks on the working directory if present
-if [ -d .snakemake ]
+if [ -z ${SBATCH_TIMELIMIT+x} ]
 then
-	snakemake --unlock
-	rm -rf .snakemake/locks/*
+	time_lim=2159
+else
+	time_lim=$SBATCH_TIMELIMIT
 fi
 
-dvc run -d setup/cohort-data.p -d setup/muts-list.p -d setup/feat-list.p \
-	-d $RUNDIR/fit_threshold.py -o $FINALDIR/out-data__${cohort}__${classif}.p.gz \
-	-f output.dvc --overwrite-dvcfile --no-commit \
-	'snakemake -s $RUNDIR/Snakefile -j 120 --latency-wait 120 \
-	--cluster-config $RUNDIR/cluster.json --cluster "sbatch -p {cluster.partition} \
-	-J {cluster.job-name} -t {cluster.time} -o {cluster.output} -e {cluster.error} \
-	-n {cluster.ntasks} -c {cluster.cpus-per-task} --mem-per-cpu {cluster.mem-per-cpu} \
-	--exclude=$ex_nodes --no-requeue" --config cohort='"$cohort"' \
-	classif='"$classif"' task_count='"$task_count"
+cur_time=$( date +%s )
+time_left=$(( time_lim - (cur_time - start_time) / 60 + 1 ))
+
+if [ -z ${time_max+x} ]
+then
+	time_max=$(( $time_left * 11 / 13 ))
+fi
+
+if [ ! -f setup/tasks.txt ]
+then
+	merge_max=$(( $time_left - $time_max - 3 ))
+
+	if [ $classif == 'Ridge' ]
+	then
+		task_size=0.91
+		samp_exp=0.5
+	fi
+
+	eval "$( python -m dryads-research.experiments.utilities.pipeline_setup \
+		$OUTDIR $time_max --merge_max=$merge_max \
+		--task_size=$task_size --samp_exp=$samp_exp )"
+fi
+
+# if we are only enumerating, we quit before classification jobs are launched
+if $count_only
+then
+	cp setup/cohort-data.p.gz $FINALDIR/cohort-data__${out_tag}.p.gz
+	exit 0
+fi
+
+eval "$( tail -n 2 setup/tasks.txt | head -n 1 )"
+eval "$( tail -n 1 setup/tasks.txt )"
+
+
+dvc run -d setup/muts-list.p -d $RUNDIR/fit_threshold.py -O out-conf.p.gz \
+	-O $FINALDIR/out-conf__${cohort}__${classif}.p.gz -f output.dvc \
+	--overwrite-dvcfile --ignore-build-cache 'snakemake -s $RUNDIR/Snakefile \
+	-j 400 --latency-wait 120 --cluster-config $RUNDIR/cluster.json \
+	--cluster "sbatch -p {cluster.partition} -J {cluster.job-name} \
+	-t {cluster.time} -o {cluster.output} -e {cluster.error} \
+	-n {cluster.ntasks} -c {cluster.cpus-per-task} \
+	--mem-per-cpu {cluster.mem-per-cpu} --exclude=$ex_nodes --no-requeue" \
+	--config cohort='"$cohort"' classif='"$classif"' \
+	time_max='"$run_time"' merge_max='"$merge_time"
 
 cp output.dvc $FINALDIR/output__${cohort}__${classif}.dvc
 
