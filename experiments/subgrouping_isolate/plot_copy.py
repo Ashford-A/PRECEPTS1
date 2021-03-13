e98ed8829106614ef9e7cbd7d16f8a2550eb455d
Michal Grzadkowski <mgrzad@gmail.com>
2021-03-13 00:38:10 -0800

    Refactoring how output data is loaded into subgrouping isolation plotting scripts that use all available cohorts.

--- experiments/subgrouping_isolate/plot_copy.py
+++ experiments/subgrouping_isolate/plot_copy.py
@@ -4,8 +4,8 @@ from ..utilities.mutations import (pnt_mtype, shal_mtype, deep_mtype,
 from dryadic.features.mutations import MuType
 
 from ..subgrouping_isolate import base_dir, train_cohorts
-from .utils import (siml_fxs, cna_mtypes, remove_pheno_dups,
-                    get_mut_ex, choose_subtype_colour)
+from .utils import (load_cohorts_data, siml_fxs, cna_mtypes,
+                    remove_pheno_dups, get_mut_ex, choose_subtype_colour)
 from ..utilities.colour_maps import variant_clrs
 from ..utilities.labels import get_cohort_label, get_fancy_label
 from ..utilities.misc import get_label, get_subtype, choose_label_colour
@@ -14,8 +14,6 @@ from ..utilities.label_placement import place_scatter_labels
 import os
 import argparse
 from pathlib import Path
-import bz2
-import dill as pickle
 from itertools import permutations as permt
 
 import numpy as np
@@ -917,6 +915,7 @@ def main():
 
     parser.add_argument('--cores', '-c', type=int, default=1)
     parser.add_argument('--seed', type=int)
+    parser.add_argument('--data_cache')
     parser.add_argument('--verbose', '-v', action='count', default=0)
 
     # parse command line arguments, find completed runs for this classifier
@@ -939,87 +938,8 @@ def main():
                          "combination of parameters!")
 
     out_list = out_list[out_list.Cohort.isin(train_cohorts)]
-    use_iter = out_list.groupby(['Source', 'Cohort', 'Levels'])['File']
-
-    out_dirs = {(src, coh): Path(base_dir, '__'.join([src, coh]))
-                for src, coh, _ in use_iter.groups}
-    out_tags = {fl: '__'.join(fl.parts[-1].split('__')[1:])
-                for fl in out_list.File}
-    pred_tag = "out-pred_{}".format(args.ex_lbl)
-
-    phn_dicts = {(src, coh): dict() for src, coh, _ in use_iter.groups}
-    cdata_dict = {(src, coh): None for src, coh, _ in use_iter.groups}
-
-    auc_lists = {(src, coh): pd.Series(dtype='float')
-                 for src, coh, _ in use_iter.groups}
-    pred_dfs = {(src, coh): pd.DataFrame() for src, coh, _ in use_iter.groups}
-
-    for (src, coh, lvls), out_files in use_iter:
-        out_aucs = list()
-        out_preds = list()
-
-        for out_file in out_files:
-            with bz2.BZ2File(Path(out_dirs[src, coh],
-                                  '__'.join(["out-pheno",
-                                             out_tags[out_file]])),
-                             'r') as f:
-                phn_dicts[src, coh].update(pickle.load(f))
-
-            with bz2.BZ2File(Path(out_dirs[src, coh],
-                                  '__'.join(["out-aucs",
-                                             out_tags[out_file]])),
-                             'r') as f:
-                out_aucs += [pickle.load(f)[args.ex_lbl]['mean']]
-
-            with bz2.BZ2File(Path(out_dirs[src, coh],
-                                  '__'.join([pred_tag, out_tags[out_file]])),
-                             'r') as f:
-                pred_vals = pickle.load(f)
-
-            out_preds += [pred_vals.applymap(np.mean)]
-
-            with bz2.BZ2File(Path(out_dirs[src, coh],
-                                  '__'.join(["cohort-data",
-                                             out_tags[out_file]])),
-                             'r') as f:
-                new_cdata = pickle.load(f)
-
-            if cdata_dict[src, coh] is None:
-                cdata_dict[src, coh] = new_cdata
-            else:
-                cdata_dict[src, coh].merge(new_cdata)
-
-        mtypes_comp = np.greater_equal.outer(
-            *([[set(auc_vals.index) for auc_vals in out_aucs]] * 2))
-        super_comp = np.apply_along_axis(all, 1, mtypes_comp)
-
-        # if there is not a subgrouping set that contains all the others,
-        # concatenate the output of all sets...
-        if not super_comp.any():
-            auc_lists[src, coh] = auc_lists[src, coh].append(
-                pd.concat(out_aucs, sort=False))
-            pred_dfs[src, coh] = pd.concat(
-                [pred_dfs[src, coh], *out_preds], sort=False)
-
-        # ...otherwise, use the "superset"
-        else:
-            super_indx = super_comp.argmax()
-
-            auc_lists[src, coh] = auc_lists[src, coh].append(
-                out_aucs[super_indx])
-            pred_dfs[src, coh] = pd.concat(
-                [pred_dfs[src, coh], out_preds[super_indx]], sort=False)
-
-    # filter out duplicate subgroupings due to overlapping search criteria
-    for src, coh, _ in use_iter.groups:
-        auc_lists[src, coh].sort_index(inplace=True)
-        pred_dfs[src, coh].sort_index(inplace=True)
-        assert (auc_lists[src, coh].index == pred_dfs[src, coh].index).all()
-
-        auc_lists[src, coh] = auc_lists[src, coh].loc[
-            ~auc_lists[src, coh].index.duplicated()]
-        pred_dfs[src, coh] = pred_dfs[src, coh].loc[
-            ~pred_dfs[src, coh].index.duplicated()]
+    pred_dfs, phn_dicts, auc_lists, cdata_dict = load_cohorts_data(
+        out_list, args.ex_lbl, args.data_cache)
 
     for siml_metric in args.siml_metrics:
         if args.auc_cutoff < 1:
--- experiments/subgrouping_isolate/utils.py
+++ experiments/subgrouping_isolate/utils.py
@@ -4,6 +4,13 @@ from ..utilities.mutations import (
 from ..utilities.metrics import calculate_mean_siml, calculate_ks_siml
 from ..utilities.colour_maps import variant_clrs, mcomb_clrs
 from ..utilities.labels import get_fancy_label
+from ..subgrouping_isolate import base_dir, train_cohorts
+
+import numpy as np
+import pandas as pd
+import bz2
+import dill as pickle
+from pathlib import Path
 
 siml_fxs = {'mean': calculate_mean_siml, 'ks': calculate_ks_siml}
 cna_mtypes = {'Gain': gains_mtype, 'Loss': dels_mtype}
@@ -76,3 +83,105 @@ def get_mcomb_lbl(mcomb):
         for mtype in mcomb.mtypes
         ])
 
+
+def load_cohorts_data(out_list, ex_lbl, data_cache=None):
+    if data_cache and Path(data_cache).exists():
+        with bz2.BZ2File(data_cache, 'r') as f:
+            pred_dfs, phn_dicts, auc_lists, cdata_dict = pickle.load(f)
+
+    else:
+        pred_dfs, phn_dicts, auc_lists, cdata_dict = load_cohorts_output(
+            out_list, ex_lbl)
+
+        if data_cache:
+            with bz2.BZ2File(data_cache, 'w') as f:
+                pickle.dump((pred_dfs, phn_dicts, auc_lists, cdata_dict),
+                            f, protocol=-1)
+
+    return pred_dfs, phn_dicts, auc_lists, cdata_dict
+
+
+def load_cohorts_output(out_list, ex_lbl):
+    use_iter = out_list.groupby(['Source', 'Cohort', 'Levels'])['File']
+
+    out_dirs = {(src, coh): Path(base_dir, '__'.join([src, coh]))
+                for src, coh, _ in use_iter.groups}
+    out_tags = {fl: '__'.join(fl.parts[-1].split('__')[1:])
+                for fl in out_list.File}
+    pred_tag = "out-pred_{}".format(ex_lbl)
+
+    phn_dicts = {(src, coh): dict() for src, coh, _ in use_iter.groups}
+    cdata_dict = {(src, coh): None for src, coh, _ in use_iter.groups}
+
+    auc_lists = {(src, coh): pd.Series(dtype='float')
+                 for src, coh, _ in use_iter.groups}
+    pred_dfs = {(src, coh): pd.DataFrame() for src, coh, _ in use_iter.groups}
+
+    for (src, coh, lvls), out_files in use_iter:
+        out_aucs = list()
+        out_preds = list()
+
+        for out_file in out_files:
+            with bz2.BZ2File(Path(out_dirs[src, coh],
+                                  '__'.join(["out-pheno",
+                                             out_tags[out_file]])),
+                             'r') as f:
+                phn_dicts[src, coh].update(pickle.load(f))
+
+            with bz2.BZ2File(Path(out_dirs[src, coh],
+                                  '__'.join(["out-aucs",
+                                             out_tags[out_file]])),
+                             'r') as f:
+                out_aucs += [pickle.load(f)[ex_lbl]['mean']]
+
+            with bz2.BZ2File(Path(out_dirs[src, coh],
+                                  '__'.join([pred_tag, out_tags[out_file]])),
+                             'r') as f:
+                pred_vals = pickle.load(f)
+
+            out_preds += [pred_vals.applymap(np.mean)]
+
+            with bz2.BZ2File(Path(out_dirs[src, coh],
+                                  '__'.join(["cohort-data",
+                                             out_tags[out_file]])),
+                             'r') as f:
+                new_cdata = pickle.load(f)
+
+            if cdata_dict[src, coh] is None:
+                cdata_dict[src, coh] = new_cdata
+            else:
+                cdata_dict[src, coh].merge(new_cdata)
+
+        mtypes_comp = np.greater_equal.outer(
+            *([[set(auc_vals.index) for auc_vals in out_aucs]] * 2))
+        super_comp = np.apply_along_axis(all, 1, mtypes_comp)
+
+        # if there is not a subgrouping set that contains all the others,
+        # concatenate the output of all sets...
+        if not super_comp.any():
+            auc_lists[src, coh] = auc_lists[src, coh].append(
+                pd.concat(out_aucs, sort=False))
+            pred_dfs[src, coh] = pd.concat(
+                [pred_dfs[src, coh], *out_preds], sort=False)
+
+        # ...otherwise, use the "superset"
+        else:
+            super_indx = super_comp.argmax()
+
+            auc_lists[src, coh] = auc_lists[src, coh].append(
+                out_aucs[super_indx])
+            pred_dfs[src, coh] = pd.concat(
+                [pred_dfs[src, coh], out_preds[super_indx]], sort=False)
+
+    # filter out duplicate subgroupings due to overlapping search criteria
+    for src, coh, _ in use_iter.groups:
+        auc_lists[src, coh].sort_index(inplace=True)
+        pred_dfs[src, coh].sort_index(inplace=True)
+        assert (auc_lists[src, coh].index == pred_dfs[src, coh].index).all()
+
+        auc_lists[src, coh] = auc_lists[src, coh].loc[
+            ~auc_lists[src, coh].index.duplicated()]
+        pred_dfs[src, coh] = pred_dfs[src, coh].loc[
+            ~pred_dfs[src, coh].index.duplicated()]
+
+    return pred_dfs, phn_dicts, auc_lists, cdata_dict
