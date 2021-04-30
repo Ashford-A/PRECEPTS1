a369cedc32524276d599cf677c689e084087d19f
Michal Grzadkowski <mgrzad@gmail.com>
2021-04-29 23:58:46 -0400

    Directly plotting inferred similarities between point mutations and combinations thereof.

--- experiments/subgrouping_isolate/plot_points.py
+++ experiments/subgrouping_isolate/plot_points.py
@@ -6,18 +6,26 @@ from dryadic.features.mutations import MuType
 from ..subgrouping_isolate import base_dir, train_cohorts
 from .utils import load_cohorts_data, siml_fxs, remove_pheno_dups, get_mut_ex
 from ..utilities.misc import get_label, get_subtype, choose_label_colour
+from ..utilities.labels import get_cohort_label
 from ..utilities.label_placement import place_scatter_labels
 
 import os
 import argparse
 from pathlib import Path
+import multiprocessing as mp
+
+from itertools import product
+from itertools import permutations as permt
+from functools import reduce
+from operator import add
 
 import numpy as np
 import pandas as pd
+from scipy.stats import ks_2samp
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
+from matplotlib.lines import Line2D
 
 mpl.use('Agg')
 plt.style.use('fivethirtyeight')
@@ -208,6 +216,169 @@ def plot_overlap_divergence(pred_dfs, pheno_dicts, auc_lists,
     plt.close()
 
 
+def plot_overlap_synergy(pred_dfs, pheno_dicts, auc_lists,
+                         cdata_dict, args, siml_metric):
+    fig, ax = plt.subplots(figsize=(11, 11))
+    siml_dfs = dict()
+
+    # for each dataset, find the subgroupings meeting the minimum task AUC
+    # that are exclusively defined and subsets of point mutations...
+    for (src, coh), auc_list in auc_lists.items():
+        use_combs = remove_pheno_dups({
+            mut for mut, auc_val in auc_list.iteritems()
+            if (isinstance(mut, ExMcomb) and auc_val >= args.auc_cutoff
+                and get_mut_ex(mut) == args.ex_lbl
+                and all(pnt_mtype.is_supertype(get_subtype(mtype))
+                        for mtype in mut.mtypes))
+            }, pheno_dicts[src, coh])
+
+        use_scombs = {mcomb for mcomb in use_combs if len(mcomb.mtypes) == 1}
+        use_dcombs = {mcomb for mcomb in use_combs if len(mcomb.mtypes) > 1}
+
+        # find all pairs of subgroupings from the same gene that are disjoint
+        # either by construction or by phenotype
+        use_pairs = {
+            (scomb, dcomb) for scomb, dcomb in product(use_scombs, use_dcombs)
+            if (get_label(scomb) == get_label(dcomb)
+                and (all((mtp1 & mtp2).is_empty()
+                         for mtp1, mtp2 in product(scomb.mtypes,
+                                                   dcomb.mtypes))
+                     or not (pheno_dicts[src, coh][scomb]
+                             & pheno_dicts[src, coh][dcomb]).any()))
+            }
+
+        # skip this dataset for plotting if we cannot find any such pairs
+        if not use_pairs:
+            continue
+
+        base_mtree = tuple(cdata_dict[src, coh].mtrees.values())[0]
+        use_genes = {get_label(mcomb)
+                     for comb_pair in use_pairs for mcomb in comb_pair}
+
+        all_mtypes = {
+            gene: MuType({('Gene', gene): base_mtree[gene].allkey()})
+            for gene in use_genes
+            }
+
+        if args.ex_lbl == 'IsoShal':
+            for gene in use_genes:
+                all_mtypes[gene] -= MuType({('Gene', gene): shal_mtype})
+
+        all_phns = {
+            gene: np.array(cdata_dict[src, coh].train_pheno(all_mtype))
+            for gene, all_mtype in all_mtypes.items()
+            }
+
+        train_samps = cdata_dict[src, coh].get_train_samples()
+        pair_combs = set(reduce(add, use_pairs))
+        use_preds = pred_dfs[src, coh].loc[pair_combs, train_samps]
+        map_args = []
+
+        wt_vals = {mcomb: use_preds.loc[mcomb][~all_phns[get_label(mcomb)]]
+                   for mcomb in pair_combs}
+        mut_vals = {mcomb: use_preds.loc[mcomb, pheno_dicts[src, coh][mcomb]]
+                    for mcomb in pair_combs}
+
+        if siml_metric == 'mean':
+            wt_means = {mcomb: vals.mean() for mcomb, vals in wt_vals.items()}
+            mut_means = {mcomb: vals.mean()
+                         for mcomb, vals in mut_vals.items()}
+
+            map_args += [
+                (wt_vals[mcomb1], mut_vals[mcomb1],
+                 use_preds.loc[mcomb1, pheno_dicts[src, coh][mcomb2]],
+                 wt_means[mcomb1], mut_means[mcomb1], None)
+                for mcombs in use_pairs for mcomb1, mcomb2 in permt(mcombs)
+                ]
+
+        elif siml_metric == 'ks':
+            base_dists = {
+                mcomb: ks_2samp(wt_vals[mcomb], mut_vals[mcomb],
+                                alternative='greater').statistic
+                for mcomb in pair_combs
+                }
+
+            map_args += [
+                (wt_vals[mcomb1], mut_vals[mcomb1],
+                 use_preds.loc[mcomb1, pheno_dicts[src, coh][mcomb2]],
+                 base_dists[mcomb1])
+                for mcombs in use_pairs for mcomb1, mcomb2 in permt(mcombs)
+                ]
+
+        if siml_metric == 'mean':
+            chunk_size = int(len(map_args) / (11 * args.cores)) + 1
+        elif siml_metric == 'ks':
+            chunk_size = int(len(map_args) / (3 * args.cores)) + 1
+
+        pool = mp.Pool(args.cores)
+        siml_list = pool.starmap(siml_fxs[siml_metric], map_args, chunk_size)
+        pool.close()
+
+        siml_dfs[src, coh] = pd.concat({(src, coh): pd.DataFrame(
+            dict(zip(use_pairs, zip(siml_list[::2], siml_list[1::2])))
+            ).transpose()})
+        siml_dfs[src, coh].index.set_names(
+            ['Source', 'Cohort', 'Mcomb1', 'Mcomb2'], inplace=True)
+
+    plt_df = pd.concat(siml_dfs.values())
+    gene_counts = plt_df.groupby(
+        lambda x: (x[0], x[1], get_label(x[2]), get_label(x[3]))).count()
+
+    plt_genes = {gene for _, _, gene, _ in gene_counts.index}
+    clr_dict = {gene: choose_label_colour(gene) for gene in plt_genes}
+    size_mult = plt_df.shape[0] ** -0.23
+
+    lgnd_lbls = ["{} in {}{:<7} pairs".format(gene, get_cohort_label(coh),
+                                              count[0])
+                 for (_, coh, gene, _), count in gene_counts.iterrows()]
+
+    lgnd_mrks = [Line2D([], [], marker='o', linestyle='None',
+                        markersize=25, alpha=0.61,
+                        markerfacecolor=clr_dict[gene],
+                        markeredgecolor='none')
+                 for _, _, gene, _ in gene_counts.index]
+
+    for (src, coh, scomb, dcomb), siml_vals in plt_df.iterrows():
+        cur_gene = get_label(scomb)
+
+        plt_size = (np.mean(pheno_dicts[src, coh][scomb])
+                    * np.mean(pheno_dicts[src, coh][dcomb])) ** 0.5
+        plt_size *= size_mult
+
+        ax.scatter(*siml_vals, s=2307 * plt_size,
+                   c=[clr_dict[cur_gene]], alpha=0.23, edgecolor='none')
+
+    ax.grid(alpha=0.47, linewidth=0.9)
+    ax.xaxis.set_major_locator(plt.MaxNLocator(5, steps=[1, 2, 5]))
+    ax.yaxis.set_major_locator(plt.MaxNLocator(5, steps=[1, 2, 5]))
+
+    plt_min, plt_max = np.percentile(plt_df.values, q=[0, 100])
+    plt_rng = plt_max - plt_min
+    plt_lims = [plt_min - plt_rng / 31, plt_max + plt_rng / 31]
+
+    ax.plot(plt_lims, plt_lims,
+            color='#550000', linewidth=1.3, linestyle='--', alpha=0.41)
+
+    ax.set_xlabel("Singletons' Similarity to Overlaps",
+                  size=25, weight='bold')
+    ax.set_ylabel("Overlaps' Similarity to Singletons",
+                  size=25, weight='bold')
+
+    ax.legend(lgnd_mrks, lgnd_lbls, bbox_to_anchor=(0.5, -0.083),
+              frameon=False, fontsize=21, ncol=1, loc=9, handletextpad=0.3)
+
+    ax.set_xlim(plt_lims)
+    ax.set_ylim(plt_lims)
+
+    plt.savefig(os.path.join(plot_dir,
+                             "{}_{}-overlap-synergy_{}.svg".format(
+                                 args.ex_lbl, siml_metric, args.classif)),
+                bbox_inches='tight', format='svg')
+
+    plt.close()
+    return siml_dfs
+
+
 def main():
     parser = argparse.ArgumentParser(
         'plot_point',
@@ -256,6 +427,9 @@ def main():
         if args.auc_cutoff < 1:
             plot_overlap_divergence(pred_dfs, phn_dicts, auc_lists,
                                     cdata_dict, args, siml_metric)
+
+            siml_dfs = plot_overlap_synergy(pred_dfs, phn_dicts, auc_lists,
+                                            cdata_dict, args, siml_metric)
 
 
 if __name__ == '__main__':
