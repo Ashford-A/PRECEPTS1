6d29aa52929a2d128cae2953ba4f9f0bdffa802c
ashforda <ashforda@metal-2-0.local>
2023-10-04 17:36:52 -0700

    Trimmed some more unnecessary files/analyses.

--- experiments/beatAML_analysis/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .data_dirs import (gene_list, beataml_expr, beataml_muts, tx_map,
-                        samp_map, toil_dir, syn_root, copy_dir, annot_file)
-
-__all__ = ['gene_list', 'beataml_expr', 'beataml_muts', 'tx_map',
-           'samp_map', 'toil_dir', 'syn_root', 'copy_dir', 'annot_file']
-
